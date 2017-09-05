5a5dc1ccab30b1c28da1303c0009f3add865ea0f
Ryan Mast <mast9@llnl.gov>
2017-09-05 14:04:39 -0700

    Fix dependency install syntax error for boost

--- scripts/install-ci-dependencies.sh
+++ scripts/install-ci-dependencies.sh
@@ -13,8 +13,7 @@ git clone git://github.com/zeromq/libzmq.git
 echo "*** built zmq successfully"
 
 echo "*** build boost"
-wget -O boost_1_61_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz/download
-    && tar xzf boost_1_61_0.tar.gz
+wget -O boost_1_61_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz/download && tar xzf boost_1_61_0.tar.gz
 (
     cd boost_1_61_0/;
     ./bootstrap.sh --with-libraries=date_time,filesystem,program_options,system,test;
@@ -24,6 +23,5 @@ wget -O boost_1_61_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.
 echo "*** built boost successfully"
 
 if [[ ! -f "cmake-3.4.3-Linux-x86_64/bin/cmake" ]]; then
-    wget --no-check-certificate http://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.tar.gz
-        && tar -xzf cmake-3.4.3-Linux-x86_64.tar.gz;
+    wget --no-check-certificate http://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64.tar.gz && tar -xzf cmake-3.4.3-Linux-x86_64.tar.gz;
 fi
