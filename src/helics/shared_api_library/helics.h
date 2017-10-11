42d7133df8384af3c667ace0b9a4ebd97e87bd98
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2017-10-11 10:32:23 -0600

    Align definitions

--- src/helics/shared_api_library/helics.h
+++ src/helics/shared_api_library/helics.h
@@ -48,11 +48,13 @@ HELICS_Export helics_core helicsCreateCoreFromArgs (const char *type, const char
 HELICS_Export helics_broker helicsCreateBroker (const char *type, const char *name, const char *initString);
 HELICS_Export helics_broker helicsCreateBrokerFromArgs (const char *type, const char *name, int argc, char *argv[]);
 
-  HELICS_Export int helicsCoreIsConnected(helics_core core);
+HELICS_Export int helicsBrokerIsConnected(helics_broker broker);
 
-  HELICS_Export void helicsFreeCore(helics_core core);
+HELICS_Export int helicsCoreIsConnected(helics_core core);
 
-  HELICS_Export void helicsFreeBroker(helics_broker broker);
+HELICS_Export void helicsFreeCore(helics_core core);
+
+HELICS_Export void helicsFreeBroker(helics_broker broker);
 
 /* Creation and destruction of Federates */
 HELICS_Export helics_value_federate helicsCreateValueFederate (const helics_federate_info_t fi);
