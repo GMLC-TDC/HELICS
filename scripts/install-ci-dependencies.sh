d759b546aa3cbdcc37417f5fc5e337119b50e33f
Ryan Mast <nightlark@users.noreply.github.com>
2017-10-30 22:59:02 -0700

    Build libzmq with cmake instead of autotools

--- scripts/install-ci-dependencies.sh
+++ scripts/install-ci-dependencies.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-echo "*** build libzmq"
-git clone git://github.com/zeromq/libzmq.git
-(
-    cd libzmq;
-    ./autogen.sh;
-    ./configure;
-    make check;
-    sudo make install;
-    sudo ldconfig
-)
-echo "*** built zmq successfully"
-
-echo "*** build boost"
-wget -O boost_1_61_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz/download && tar xzf boost_1_61_0.tar.gz
-(
-    cd boost_1_61_0/;
-    ./bootstrap.sh --with-libraries=date_time,filesystem,program_options,system,test;
-    ./b2 link=shared threading=multi variant=release > /dev/null;
-    sudo ./b2 install > /dev/null
-)
-echo "*** built boost successfully"
-
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     if [[ ! -f "cmake-3.4.3-Linux-x86_64/bin/cmake" ]]; then
         echo "*** install cmake"
@@ -38,4 +16,25 @@ elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     echo "*** cmake installed ($PATH)"
 fi
 
-cmake --version
+echo "*** build libzmq"
+git clone git://github.com/zeromq/libzmq.git
+(
+    cd libzmq;
+    ./autogen.sh;
+    mkdir build && cd build;
+    cmake .. -DWITH_PERF_TOOL=OFF -DZMQ_BUILD_TESTS=OFF -DENABLE_CPACK=OFF -DCMAKE_BUILD_TYPE=Release
+    make;
+    sudo make install;
+    sudo ldconfig
+)
+echo "*** built zmq successfully"
+
+echo "*** build boost"
+wget -O boost_1_61_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz/download && tar xzf boost_1_61_0.tar.gz
+(
+    cd boost_1_61_0/;
+    ./bootstrap.sh --with-libraries=date_time,filesystem,program_options,system,test;
+    ./b2 link=shared threading=multi variant=release > /dev/null;
+    sudo ./b2 install > /dev/null
+)
+echo "*** built boost successfully"
