cd92c103b99df82d8a7d72c8516bfabef4df8d3f
Ryan Mast <nightlark@users.noreply.github.com>
2017-11-07 14:34:35 -0800

    Dependency cache checks for dirs instead of files

--- scripts/install-ci-dependencies.sh
+++ scripts/install-ci-dependencies.sh
@@ -12,7 +12,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     export PATH="${PWD}/cmake-3.4.3-Linux-x86_64/bin:${PATH}"
     echo "*** cmake installed ($PATH)"
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    if [[ ! -f "cmake-3.4.3-Darwin-x86_64/CMake.app" ]]; then
+    if [[ ! -d "cmake-3.4.3-Darwin-x86_64/CMake.app" ]]; then
         echo "*** install cmake"
         wget --no-check-certificate http://cmake.org/files/v3.4/cmake-3.4.3-Darwin-x86_64.tar.gz && tar -xzf cmake-3.4.3-Darwin-x86_64.tar.gz;
     fi
@@ -20,7 +20,7 @@ elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     echo "*** cmake installed ($PATH)"
 fi
 
-if [[ ! -f "dependencies/zmq" ]]; then
+if [[ ! -d "dependencies/zmq" ]]; then
     echo "*** build libzmq"
     git clone git://github.com/zeromq/libzmq.git
     (
@@ -34,7 +34,7 @@ if [[ ! -f "dependencies/zmq" ]]; then
     echo "*** built zmq successfully"
 fi
 
-if [[ ! -f "dependencies/boost" ]]; then
+if [[ ! -d "dependencies/boost" ]]; then
     echo "*** build boost"
     wget -O boost_1_61_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz/download && tar xzf boost_1_61_0.tar.gz
     (
