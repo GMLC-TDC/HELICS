e3ae8af7533b4ce28cedd62ab986efcf47b7d62b
Ryan Mast <nightlark@users.noreply.github.com>
2017-11-07 09:53:23 -0800

    Check for cached copy of CMake on Mac builds

--- scripts/install-ci-dependencies.sh
+++ scripts/install-ci-dependencies.sh
@@ -8,7 +8,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     export PATH="${PWD}/cmake-3.4.3-Linux-x86_64/bin:${PATH}"
     echo "*** cmake installed ($PATH)"
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    if [[ ! -f "cmake-3.4.3-Darwin-x86_64/bin/cmake" ]]; then
+    if [[ ! -f "cmake-3.4.3-Darwin-x86_64/CMake.app" ]]; then
         echo "*** install cmake"
         wget --no-check-certificate http://cmake.org/files/v3.4/cmake-3.4.3-Darwin-x86_64.tar.gz && tar -xzf cmake-3.4.3-Darwin-x86_64.tar.gz;
     fi
