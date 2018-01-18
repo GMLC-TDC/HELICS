aada23eab624b99f906ea134aaa79fab523a7ce9
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-01-18 10:29:56 -0700

    Update helicsVersion.hpp

--- src/helics/core/helicsVersion.hpp
+++ src/helics/core/helicsVersion.hpp
@@ -3,7 +3,9 @@
 Copyright (C) 2017-2018, Battelle Memorial Institute
 All rights reserved.
 
-This software was co-developed by Pacific Northwest National Laboratory, operated by the Battelle Memorial Institute; the National Renewable Energy Laboratory, operated by the Alliance for Sustainable Energy, LLC; and the Lawrence Livermore National Laboratory, operated by Lawrence Livermore National Security, LLC.
+This software was co-developed by Pacific Northwest National Laboratory, operated by the Battelle Memorial
+Institute; the National Renewable Energy Laboratory, operated by the Alliance for Sustainable Energy, LLC; and the
+Lawrence Livermore National Laboratory, operated by Lawrence Livermore National Security, LLC.
 
 */
 #ifndef _HELICS_VERSION_
@@ -19,15 +21,14 @@ file linking with version info and containing some convenience functions
 namespace helics
 {
 /** @returns a string containing version information*/
-std::string helicsVersionString();
+std::string helicsVersionString ();
 
 /** get the Major version number*/
-inline int helicsVersionMajor() { return HELICS_VERSION_MAJOR; }
+inline int helicsVersionMajor () { return HELICS_VERSION_MAJOR; }
 /** get the Minor version number*/
-inline int helicsVersionMinor() { return HELICS_VERSION_MINOR; }
+inline int helicsVersionMinor () { return HELICS_VERSION_MINOR; }
 /** get the patch number*/
-inline int helicsVersionPatch() { return HELICS_VERSION_PATCH; }
+inline int helicsVersionPatch () { return HELICS_VERSION_PATCH; }
 }
 
-
-#endif _HELICS_VERSION_
+#endif /*_HELICS_VERSION_*/
