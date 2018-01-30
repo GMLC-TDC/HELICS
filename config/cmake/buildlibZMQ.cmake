2f5ef6c7e70b27a9314e73fb39092364f38f87d2
Philip Top <phlptp@gmail.com>
2018-01-30 06:36:13 -0800

    improvements to buildZMQ on linux

--- config/cmake/buildlibZMQ.cmake
+++ config/cmake/buildlibZMQ.cmake
@@ -17,8 +17,10 @@ set(trigger_build_dir ${CMAKE_BINARY_DIR}/autobuild/force_libzmq)
 	escape_string(cxx_compiler_string ${CMAKE_CXX_COMPILER})
 	escape_string(c_compiler_string ${CMAKE_C_COMPILER})
 	escape_string(linker_string ${CMAKE_LINKER})
+	message(STATUS "cxx compiler ${cxx_compiler_string}")
+	message(STATUS "linker ${linker_string}")
 	
-	set(extra_cxx_flagss "")
+	set(extra_cxx_flags ${CMAKE_CXX_FLAGS})
 	if (UNIX)
 	if (USE_LIBCXX)
 		set(extra_cxx_flags "${extra_cxx_flags} -stdlib=libc++")
@@ -26,9 +28,20 @@ set(trigger_build_dir ${CMAKE_BINARY_DIR}/autobuild/force_libzmq)
 	else(UNIX)
 	endif()
 	
+	if (ZMQ_USE_STATIC_LIBRARY)
+	   set(zmq_static_build ON)
+	   set(zmq_shared_build OFF)
+	   set(extra_cxx_flags "${extra_cxx_flags} -fPIC")
+	else()
+        set(zmq_static_build OFF)
+	   set(zmq_shared_build ON)
+	endif()
+	
 	if (VERSION_OPTION)
 		set(extra_cxx_flags "${extra_cxx_flags} ${VERSION_OPTION}")
 	endif()
+	
+	message(STATUS "flags ${extra_cxx_flags}")
 	
     #mktemp dir in build tree
     file(MAKE_DIRECTORY ${trigger_build_dir} ${trigger_build_dir}/build)
@@ -46,15 +59,17 @@ ExternalProject_Add(libzmq
 	BINARY_DIR ${PROJECT_BINARY_DIR}/ThirdParty/libzmq
 	 
     CMAKE_ARGS 
+        -DCMAKE_CXX_COMPILER=${cxx_compiler_string}
+        -DCMAKE_C_COMPILER=${c_compiler_string}
         -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/libs
         -DCMAKE_BUILD_TYPE=\$\{CMAKE_BUILD_TYPE\}
 		-DZMQ_BUILD_TESTS=OFF
-		-DCMAKE_CXX_FLAGS=\"${extra_cxx_flags}\"
+		-DBUILD_STATIC=${zmq_static_build}
+		-DBUILD_SHARED=${zmq_shared_build}
+		\"-DCMAKE_CXX_FLAGS=${extra_cxx_flags}\"
+		\"-DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}\"
 		-DENABLE_CPACK=OFF
 		-DLIBZMQ_PEDANTIC=OFF
-        -DCMAKE_CXX_COMPILER=${cxx_compiler_string}
-        -DCMAKE_C_COMPILER=${c_compiler_string}
-        -DCMAKE_LINKER=${linker_string}
 		-DZEROMQ_CMAKECONFIG_INSTALL_DIR=${PROJECT_BINARY_DIR}/libs/cmake/ZeroMQ
 		
 	INSTALL_DIR ${PROJECT_BINARY_DIR}/libs
@@ -64,8 +79,8 @@ ExternalProject_Add(libzmq
     file(WRITE ${trigger_build_dir}/CMakeLists.txt "${CMAKE_LIST_CONTENT}")
 
 	message(STATUS "Configuring ZeroMQ Autobuild for release logging to ${PROJECT_BINARY_DIR}/logs/zmq_autobuild_config_release.log")
-    execute_process(COMMAND ${CMAKE_COMMAND}  -D CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -D CMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-	    -D CMAKE_BUILD_TYPE=Release -G ${CMAKE_GENERATOR} .. 
+    execute_process(COMMAND ${CMAKE_COMMAND}  -D CMAKE_CXX_COMPILER=${cxx_compiler_string} -D CMAKE_C_COMPILER=${c_compiler_string}
+	    -D CMAKE_LINKER=${linker_string} -D CMAKE_BUILD_TYPE=Release -G ${CMAKE_GENERATOR} .. 
         WORKING_DIRECTORY ${trigger_build_dir}/build
 		OUTPUT_FILE ${PROJECT_BINARY_DIR}/logs/zmq_autobuild_config_release.log
         )
