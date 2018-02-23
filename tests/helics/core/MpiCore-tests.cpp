efd0e6b514c9574dee799c5da879e455f1e0c642
Ryan Mast <mast9@llnl.gov>
2018-02-23 14:07:23 -0800

    Remove conditional for MpiCore tests in favor of CMake conditional

--- tests/helics/core/MpiCore-tests.cpp
+++ tests/helics/core/MpiCore-tests.cpp
@@ -11,12 +11,9 @@ Lawrence Livermore National Laboratory, operated by Lawrence Livermore National 
 #include "testFixtures.h"
 #include <boost/test/unit_test.hpp>
 
-#if HELICS_HAVE_MPI
 BOOST_AUTO_TEST_SUITE (MpiCore_tests)
 //BOOST_FIXTURE_TEST_SUITE (MpiCore_tests, mpiCoreTestFixture)
 
 BOOST_AUTO_TEST_CASE (init_test) {}
 
 BOOST_AUTO_TEST_SUITE_END ()
-
-#endif
