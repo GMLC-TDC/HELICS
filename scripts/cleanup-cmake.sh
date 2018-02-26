43da5212216b2b07cd583f6cfd48df5ec1da9f65
Ryan Mast <mast9@llnl.gov>
2018-02-26 10:38:39 -0800

    Force file deletion for cmake cleanup; don't care if the file doesn't exist

--- scripts/cleanup-cmake.sh
+++ scripts/cleanup-cmake.sh
@@ -6,16 +6,16 @@ cmake_install_path=${CI_DEPENDENCY_DIR}/cmake
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     rm -rf ${cmake_install_path}/doc
     rm -rf ${cmake_install_path}/man
-    rm ${cmake_install_path}/bin/ccmake
-    rm ${cmake_install_path}/bin/cmake-gui
+    rm -f ${cmake_install_path}/bin/ccmake
+    rm -f ${cmake_install_path}/bin/cmake-gui
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     rm -rf ${cmake_install_path}/CMake.app/Contents/Frameworks
     rm -rf ${cmake_install_path}/CMake.app/Contents/MacOS
     rm -rf ${cmake_install_path}/CMake.app/Contents/Resources
     rm -rf ${cmake_install_path}/CMake.app/Contents/doc
     rm -rf ${cmake_install_path}/CMake.app/Contents/man
-    rm ${cmake_install_path}/CMake.app/Contents/bin/ccmake
-    rm ${cmake_install_path}/CMake.app/Contents/bin/cmake-gui
+    rm -f ${cmake_install_path}/CMake.app/Contents/bin/ccmake
+    rm -f ${cmake_install_path}/CMake.app/Contents/bin/cmake-gui
 fi
 
 
