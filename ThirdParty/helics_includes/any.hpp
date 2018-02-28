7a57dd8bc324f44a6f6ae37cafe135e8506fa051
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-02-28 13:26:21 -0700

    Update any.hpp to fix unicode issue

--- ThirdParty/helics_includes/any.hpp
+++ ThirdParty/helics_includes/any.hpp
@@ -8,7 +8,7 @@
 //   + https://cplusplus.github.io/LWG/lwg-active.html#2509
 //
 //
-// Copyright (c) 2016 Denilson das Mercês Amorim
+// Copyright (c) 2016 Denilson das MercÃªs Amorim
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
