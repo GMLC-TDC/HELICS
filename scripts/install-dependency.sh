19123a5c478339c6bb1064e49bb3605ccb3ac943
Ryan Mast <nightlark@users.noreply.github.com>
2018-03-02 22:50:26 -0800

    Compiler toolset options for building dependencies
    Adds option for setting boost toolset and a flag to create symlinks that get added first to system PATH for forcing compiler to use when multiple versions are present on system

--- scripts/install-dependency.sh
+++ scripts/install-dependency.sh
@@ -53,10 +53,11 @@ install_boost () {
     local boost_version=$1
     local boost_version_str=boost_${ver[0]}_${ver[1]}_${ver[2]}
     local install_path=$2
+    local boost_toolset=$3
     wget --no-check-certificate -O ${boost_version_str}.tar.gz http://sourceforge.net/projects/boost/files/boost/${boost_version}/${boost_version_str}.tar.gz/download && tar xzf ${boost_version_str}.tar.gz
     (
         cd ${boost_version_str}/;
-        ./bootstrap.sh --with-libraries=date_time,filesystem,program_options,system,chrono,timer,test;
+        ./bootstrap.sh --with-toolset=${boost_toolset} --with-libraries=date_time,filesystem,program_options,system,chrono,timer,test;
         ./b2 link=shared threading=multi variant=release > /dev/null;
         ./b2 install --prefix=${install_path} > /dev/null;
     )
@@ -85,9 +86,36 @@ install_cmake () {
 
 install_version=$2
 install_path=$3
+
+compiler_toolset=$4
+if [[ -z $compiler_toolset ]]; then
+    compiler_toolset=gcc
+fi
+
+# If FORCE_TOOLSET is set, create symlinks and add directory to path
+# May be needed to force boost to build with the right compiler version
+if [[ "$FORCE_TOOLSET" ]]; then
+    case "${compiler_toolset}" in
+        gcc*)
+            ln -s $(which ${CC}) gcc
+            ln -s $(which ${CXX}) g++
+            ;;
+        clang*)
+            ln -s $(which ${CC}) clang
+            ln -s ${which ${CXX}) clang++
+            ;;
+        intel*)
+            ln -s $(which ${CXX}) icc
+            ;;
+        *)
+            echo "Unrecognized compiler toolset"
+    esac
+    PATH=$(pwd):$PATH
+fi
+
 case "$1" in
     boost)
-        install_boost ${install_version} ${install_path}
+        install_boost ${install_version} ${install_path} ${compiler_toolset}
         ;;
     cmake)
         install_cmake ${install_version} ${install_path}
@@ -101,6 +129,7 @@ case "$1" in
         ;;
     *)
         echo "Usage:"
-        echo "$0 (boost|cmake|swig) version install_path"
+        echo "$0 (cmake|swig) version install_path"
+        echo "$0 boost version install_path [toolset=gcc]"
         echo "$0 zmq install_path"
 esac
