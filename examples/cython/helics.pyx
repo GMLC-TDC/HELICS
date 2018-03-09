243428261e3e42d9c8cd2bdc00d62520400c860d
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-03-09 12:35:05 -0700

    Remove copyright from python and Makefiles

--- examples/cython/helics.pyx
+++ examples/cython/helics.pyx
@@ -1,6 +1,5 @@
 # Copyright (C) 2017, Battelle Memorial Institute
 # All rights reserved.
-# This software was co-developed by Pacific Northwest National Laboratory, operated by the Battelle Memorial Institute; the National Renewable Energy Laboratory, operated by the Alliance for Sustainable Energy, LLC; and the Lawrence Livermore National Laboratory, operated by Lawrence Livermore National Security, LLC.
 
 cimport cython
 
