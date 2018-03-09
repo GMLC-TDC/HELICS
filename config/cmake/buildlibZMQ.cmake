52fc490e290d665875dae0b05d807e066618d45a
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-03-09 12:29:13 -0700

    Remove copyright from CMakeLists.txt

--- config/cmake/buildlibZMQ.cmake
+++ config/cmake/buildlibZMQ.cmake
@@ -1,8 +1,6 @@
 ##############################################################################
 #Copyright (C) 2017, Battelle Memorial Institute
 #All rights reserved.
-
-#This software was co-developed by Pacific Northwest National Laboratory, operated by the Battelle Memorial Institute; the National Renewable Energy Laboratory, operated by the Alliance for Sustainable Energy, LLC; and the Lawrence Livermore National Laboratory, operated by Lawrence Livermore National Security, LLC.
 ##############################################################################
 # This function is used to force a build on a dependant project at cmake configuration phase.
 #
