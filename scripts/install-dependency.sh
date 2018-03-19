48a65557ef3a9b160943a29945a208262e22aa31
Ryan Mast <nightlark@users.noreply.github.com>
2018-03-19 16:10:29 -0700

    Added CI compilation options (#216)
    * Boost toolset can be set to gcc/clang/icc based on the compiler being used (#196)
    
    * C++17 flag can be set for building HELICS (and Boost)
    
    * Adds a daily clang-5 build with C++17 standard (#201)
    
    * Adds a daily gcc-5 build
    
    * install-ci-dependencies.sh can be used on non-Travis systems by passing it the install directory for dependencies as an argument -- python 3 needs to already be installed

--- scripts/install-dependency.sh
+++ scripts/install-dependency.sh
@@ -36,7 +36,7 @@ install_zmq () {
     local install_path=$2
     if [[ "${zmq_version}" == "HEAD" ]]; then
         git clone git://github.com/zeromq/libzmq.git;
-    else 
+    else
         git clone --branch v${zmq_version} git://github.com/zeromq/libzmq.git;
     fi
     (
@@ -115,11 +115,12 @@ install_boost () {
     local boost_version=$1
     local boost_version_str=boost_${ver[0]}_${ver[1]}_${ver[2]}
     local install_path=$2
+    local boost_toolset=$3
     wget --no-check-certificate -O ${boost_version_str}.tar.gz http://sourceforge.net/projects/boost/files/boost/${boost_version}/${boost_version_str}.tar.gz/download && tar xzf ${boost_version_str}.tar.gz
     (
         cd ${boost_version_str}/;
-        ./bootstrap.sh --with-libraries=date_time,filesystem,program_options,system,chrono,timer,test;
-        ./b2 link=shared threading=multi variant=release > /dev/null;
+        ./bootstrap.sh --with-toolset=${boost_toolset} --with-libraries=date_time,filesystem,program_options,system,chrono,timer,test;
+        ./b2 -j2 link=shared threading=multi variant=release cxxflags=${BOOST_CXX_FLAGS} > /dev/null;
         ./b2 install --prefix=${install_path} > /dev/null;
     )
     rm ${boost_version_str}.tar.gz
@@ -147,9 +148,42 @@ install_cmake () {
 
 install_version=$2
 install_path=$3
+
+compiler_toolset=$4
+if [[ -z $compiler_toolset ]]; then
+    compiler_toolset=gcc
+fi
+
+if [[ "$CXX_STANDARD" == 17 ]]; then
+    echo "Install dependency with C++17 flag requested"
+    BOOST_CXX_FLAGS="-std=c++17"
+fi
+
+
+# If FORCE_TOOLSET is set, create symlinks and add directory to path
+# May be needed to force boost to build with the right compiler version
+if [[ "$FORCE_TOOLSET" ]]; then
+    case "${compiler_toolset}" in
+        gcc*)
+            ln -s $(which ${CC}) gcc
+            ln -s $(which ${CXX}) g++
+            ;;
+        clang*)
+            ln -s $(which ${CC}) clang
+            ln -s $(which ${CXX}) clang++
+            ;;
+        intel*)
+            ln -s $(which ${CXX}) icc
+            ;;
+        *)
+            echo "Unrecognized compiler toolset"
+    esac
+    PATH=$(pwd):$PATH
+fi
+
 case "$1" in
     boost)
-        install_boost ${install_version} ${install_path}
+        install_boost ${install_version} ${install_path} ${compiler_toolset}
         ;;
     cmake)
         install_cmake ${install_version} ${install_path}
@@ -172,7 +206,8 @@ case "$1" in
         ;;
     *)
         echo "Usage:"
-        echo "$0 (boost|cmake|mpich|openmpi|swig) version install_path"
+        echo "$0 (cmake|mpich|openmpi|swig) version install_path"
+        echo "$0 boost version install_path [toolset=gcc]"
         echo "$0 zmq [version=HEAD] install_path"
 esac
 
