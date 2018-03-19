7dedc77c6b413bb5e41b6739300ea6a0ca9b741a
Philip Top <top1@llnl.gov>
2018-03-19 13:16:06 -0700

    update copyright on a few files to match the rest of the repo

--- src/helics/cpp98/Core.hpp
+++ src/helics/cpp98/Core.hpp
@@ -1,11 +1,7 @@
 /*
-Copyright (C) 2017-2018, Battelle Memorial Institute
-All rights reserved.
-
-This software was co-developed by Pacific Northwest National Laboratory, operated by the Battelle Memorial
-Institute; the National Renewable Energy Laboratory, operated by the Alliance for Sustainable Energy, LLC; and the
-Lawrence Livermore National Laboratory, operated by Lawrence Livermore National Security, LLC.
-
+Copyright © 2017-2018,
+Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance for Sustainable Energy, LLC
+All rights reserved. See LICENSE file and DISCLAIMER for more details.
 */
 #ifndef HELICS_CPP98_CORE_HPP_
 #define HELICS_CPP98_CORE_HPP_
