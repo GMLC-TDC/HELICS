8447c5c0df3641f018ebe0231cd2383ca37a9700
Philip Top <phlptp@gmail.com>
2018-04-20 17:15:34 -0700

    add cassert

--- src/helics/core/CommsInterface.cpp
+++ src/helics/core/CommsInterface.cpp
@@ -1,12 +1,11 @@
 /*
-
 Copyright © 2017-2018,
 Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance for Sustainable Energy, LLC
 All rights reserved. See LICENSE file and DISCLAIMER for more details.
 */
 #include "CommsInterface.hpp"
 #include "NetworkBrokerData.hpp"
-
+#include <cassert>
 namespace helics
 {
 CommsInterface::CommsInterface (const std::string &localTarget, const std::string &brokerTarget)
