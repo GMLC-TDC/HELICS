b13eb0c3d8fb8c2cd48ca293fe7d8d28d9df1b5b
Philip Top <phlptp@gmail.com>
2018-05-08 08:00:14 -0700

    start work on updating the query capabilities in HELICS.  This commit cleans up some code and adds a few separate  functions.  No capability changes.

--- src/helics/core/ActionMessageDefintions.hpp
+++ src/helics/core/ActionMessageDefintions.hpp
@@ -45,6 +45,7 @@ enum class action_t : int32_t
     cmd_reg_fed = -105,  //!< register a federate
     cmd_priority_ack = -254,  //!< priority commands usually have an ack this is an ack that doesn't do anything
     cmd_query = -cmd_info_basis - 37,  //!< send a query this is a priority command
+    cmd_broker_query=-37, //!< send a query to a core
     cmd_query_reply = -cmd_info_basis - 38,  //!< response to a query
     cmd_reg_broker = -cmd_info_basis - 40,  //!< for a broker to connect with a higher level broker
 
@@ -206,6 +207,7 @@ enum class action_t : int32_t
 #define CMD_PRIORITY_ACK action_message_def::action_t::cmd_priority_ack
 
 #define CMD_QUERY action_message_def::action_t::cmd_query
+#define CMD_BROKER_QUERY action_message_def::action_t::cmd_broker_query
 #define CMD_QUERY_REPLY action_message_def::action_t::cmd_query_reply
 
 // definitions for the protocol options
