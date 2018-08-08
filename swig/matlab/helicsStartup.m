667ffead62200c777cec983a7908af650dabd6e3
Bryan Palmintier <bryan.palmintier@nrel.gov>
2018-08-07 20:21:06 -0700

    matlab fixes: clean and comment pireceiver, fix typo in startup

--- swig/matlab/helicsStartup.m
+++ swig/matlab/helicsStartup.m
@@ -1,43 +1,53 @@
-disp('Loading HELICS ...')
-if (exists('helicsLibPath','var')==1)
-    directory=helicsLibPath;
-else
-    directory = fileparts(mfilename('fullpath'));
+function success = helicsStartup(helicsLibPath)
+% HELICSSTARTUP configures MATLAB for HELICS use 
+
+if nargin < 1
+    %If not specified use our location, which should have required lib once
+    %compiled/installed
+    helicsLibPath = fileparts(mfilename('fullpath'));
 end
 
+fprintf('Loading HELICS (from %s)...', helicsLibPath)
 
-listing = dir(fullfile(directory, '*helicsSharedLib.*'));
+%% Extract HELICS library name in a cross-platform way
+listing = dir(fullfile(helicsLibPath, '*helicsSharedLib.*'));
 
 libraryName = '';
 
+%TODO: vectorize (make MATLAB-esque)
 for i=1:numel(listing)
-    if ( endsWith(listing(i).name, '.h') == 1 )
+    if endsWith(listing(i).name, '.h')
         continue;
     end
-    if ( endsWith(listing(i).name, '.lib') == 1 )
+    if endsWith(listing(i).name, '.lib')
         continue;
     end
-    librarynName = listing(i).name;
+    libraryName = listing(i).name;
 end
 
-if isempty(librarynName)
+%% Try some backups if not found
+if isempty(libraryName)
     %if we are empty try for a debug version
     listing = dir(fullfile(directory, '*helicsSharedLibd.*'));
     
     for i=1:numel(listing)
-        if ( endsWith(listing(i).name, '.h') == 1 )
+        if endsWith(listing(i).name, '.h')
             continue;
         end
-        if ( endsWith(listing(i).name, '.lib') == 1 )
+        if endsWith(listing(i).name, '.lib')
             continue;
         end
-        librarynName = listing(i).name;
+        libraryName = listing(i).name;
     end
     
 end
 
 if (~isempty(libraryName))
-    loadlibrary(GetFullPath(fullfile(directory, librarynName)));
+    loadlibrary(GetFullPath(fullfile(helicsLibPath, libraryName)));
 else
-    disp('Unable to find library for helics')
+    disp('Unable to find library for HELICS')
+end
+
+if nargout >1
+    success = ~isempty(libraryName);
 end
--- swig/matlab/pireceiver.m
+++ swig/matlab/pireceiver.m
@@ -1,74 +1,103 @@
-%loadlibrary(GetFullPath('~/local/helics-develop/lib/libhelicsSharedLib.dylib'));
+% PIRECEIVER script demonstrating MATLAB-HELICS interface
+%
+% This example attempts to request the next time equal to its
+% sim_stop_time, but the corresponding pisender will request intermediate
+% times such that the pireceiver will be granted times earlier than
+% requested
 
-fedinitstring = '--federates=1';
-deltat = 0.01;
+%% Initialize HELICS library in MATLAB
+helicsStartup
 
+%% Configuration
+deltat = 0.01;  %Base time interval (seconds)
+sim_stop_time = 20;
+
+% HELICS options
+helics_core_type = 'zmq'; 
+fedinitstring = '--federates=1';  % required with current C interface when using separate processes for each federate
+
+%% Provide summary information
 helicsversion = helics.helicsGetVersion();
 
-display(strcat('PI RECEIVER: Helics version = ', helicsversion));
+fprintf('PI RECEIVER: Helics version = %s\n', helicsversion)
 
-% Create Federate Info object that describes the federate properties
+%% Create Federate Info object that describes the federate properties
 fedinfo = helics.helicsFederateInfoCreate();
 
 % Set Federate name
-status = helics.helicsFederateInfoSetFederateName(fedinfo, 'TestB Federate');
+status = helics.helicsFederateInfoSetFederateName(fedinfo, 'MATLAB Pi Receiver Federate');
+assert(status==0)
 
 % Set core type from string
-status = helics.helicsFederateInfoSetCoreTypeFromString(fedinfo, 'zmq');
+status = helics.helicsFederateInfoSetCoreTypeFromString(fedinfo, helics_core_type);
+assert(status==0)
 
 % Federate init string
 status = helics.helicsFederateInfoSetCoreInitString(fedinfo, fedinitstring);
+assert(status==0)
 
-% Set the message interval (timedelta) for federate. Note that
+% Set the message interval (timedelta) for federate. 
+% Note:
 % HELICS minimum message time interval is 1 ns and by default
 % it uses a time delta of 1 second. What is provided to the
-% setTimedelta routine is a multiplier for the default timedelta.
+% setTimedelta routine is a multiplier for the default timedelta 
+% (default unit = seconds).
 
-% Set one second message interval
+% Set one message interval
 status = helics.helicsFederateInfoSetTimeDelta(fedinfo, deltat);
+assert(status==0)
 
 status = helics.helicsFederateInfoSetLoggingLevel(fedinfo, 1);
+assert(status==0)
 
-% Create value federate
+%% Actually create value federate
 vfed = helics.helicsCreateValueFederate(fedinfo);
-display('PI RECEIVER: Value federate created');
+disp('PI RECEIVER: Value federate created');
 
-% Subscribe to PI SENDER's publication
+% Subscribe to PI SENDER's publication (note: published as global)
 sub = helics.helicsFederateRegisterSubscription(vfed, 'testA', 'double', '');
 
-display('PI RECEIVER: Subscription registered');
+disp('PI RECEIVER: Subscription registered');
 
+%% Start execution
 status = helics.helicsFederateEnterExecutionMode(vfed);
-display('PI RECEIVER: Entering execution mode');
+if not(status == 0)
+    fprintf('PI RECEIVER: Failed to enter execution mode (status = %d)\n', status);
+    disp('
+end
+
+disp('PI RECEIVER: Entering execution mode');
 
 value = 0.0;
 prevtime = 0;
 
-currenttime = helics.helicsFederateRequestTime(vfed, 20);
+currenttime = helics.helicsFederateRequestTime(vfed, sim_stop_time);
 
 isupdated = helics.helicsSubscriptionIsUpdated(sub);
 
 if (isupdated == 1)
     [result, value] = helics.helicsSubscriptionGetDouble(sub);
-    display(['PI RECEIVER: Received value = ', '', num2str(value), '', ' at time ', '', num2str(currenttime), ' from PI SENDER']);
+    fprintf('PI RECEIVER: Received value = %s at time %s from PI SENDER\n', value, currenttime);
 end
 
+%% Continue execution until end of requested simulation time
+while currenttime <= sim_stop_time
 
-while currenttime <= 20
-
-    [status, currenttime] = helics.helicsFederateRequestTime(vfed, 20);
+    [status, currenttime] = helics.helicsFederateRequestTime(vfed, sim_stop_time);
+    assert(status==0)
 
     isupdated = helics.helicsSubscriptionIsUpdated(sub);
 
     if (isupdated == 1)
         [result, value] = helics.helicsSubscriptionGetDouble(sub);
-        display(['PI RECEIVER: Received value = ', '', num2str(value), '', ' at time ', '', num2str(currenttime), ' from PI SENDER']);
+        fprintf('PI RECEIVER: Received value = %s at time %s from PI SENDER\n', value, currenttime);
     end
 end
 
 status = helics.helicsFederateFinalize(vfed);
+assert(status==0)
 
 helics.helicsFederateFree(vfed);
 helics.helicsCloseLibrary();
 
-display('PI RECEIVER: Federate finalized');
+disp('PI RECEIVER: Federate finalized');
