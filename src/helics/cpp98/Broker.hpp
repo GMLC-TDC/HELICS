51f522a1fc1625e3973d6f9d24b2e3a25a214dae
Philip Top <phlptp@gmail.com>
2018-08-28 07:11:45 -0700

    fix missing header

--- src/helics/cpp98/Broker.hpp
+++ src/helics/cpp98/Broker.hpp
@@ -9,7 +9,7 @@ All rights reserved. See LICENSE file and DISCLAIMER for more details.
 
 #include "../shared_api_library/helics.h"
 #include "config.hpp"
-#include <exception>
+#include <stdexcept>
 #include <string>
 
 namespace helics98
