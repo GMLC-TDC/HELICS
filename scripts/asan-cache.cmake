e4d5faa4c477297242d5e82cec74c3d938c54640
Ryan Mast <mast9@llnl.gov>
2018-09-05 16:02:30 -0700

    Add linker flags for sanitizers

--- scripts/asan-cache.cmake
+++ scripts/asan-cache.cmake
@@ -1,2 +1,4 @@
 set(CMAKE_C_FLAGS "-fsanitize=address -fno-omit-frame-pointer -g -O1" CACHE STRING "Flags used by the compiler during all build types.")
 set(CMAKE_CXX_FLAGS "-fsanitize=address -fno-omit-frame-pointer -g -O1" CACHE STRING "Flags used by the compiler during all build types.")
+set(CMAKE_EXE_LINKER_FLAGS "-fsanitize=address" CACHE STRING "Flags used by the linker during all build types.")
+set(CMAKE_MODULE_LINKER_FLAGS "-fsanitize=address" CACHE STRING "Flags used by the linker during all build types.")
