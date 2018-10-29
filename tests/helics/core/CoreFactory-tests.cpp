46f21661f08966ff6bf8b541235216bd8e5e509b
Philip Top <top1@llnl.gov>
2018-10-29 15:23:37 -0700

    remove core-factory-tests

--- tests/helics/core/CoreFactory-tests.cpp
+++ tests/helics/core/CoreFactory-tests.cpp
@@ -11,7 +11,7 @@ All rights reserved. See LICENSE file and DISCLAIMER for more details.
 
 namespace utf = boost::unit_test;
 
-BOOST_AUTO_TEST_SUITE (CoreFactory_tests, *utf::label("ci"))
+BOOST_AUTO_TEST_SUITE (CoreFactory_tests, *utf::label("cic"))
 
 #if HELICS_HAVE_ZEROMQ
 BOOST_AUTO_TEST_CASE (ZmqCore_test)
