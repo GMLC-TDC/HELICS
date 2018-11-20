28a4aeb4f63b1085af8e34346ef5aee660b1c538
Philip Top <top1@llnl.gov>
2018-11-20 10:25:25 -0800

    add interpreter for filter connections

--- src/helics/core/fileConnections.hpp
+++ src/helics/core/fileConnections.hpp
@@ -46,10 +46,51 @@ void makeConnectionsToml(brkX *brk, const std::string &file)
                 std::string pub = tomlGetOrDefault(conn, "publication", std::string());
                 if (conn.has("targets"))
                 {
-                    auto &targetArray = conn["targets"].as<toml::Array>();
+                    auto &targetArray = conn.get<toml::Array>("targets");
                     for (const auto &target : targetArray)
                     {
                         brk->dataLink(pub, target.as<std::string>());
+                    }
+                }
+            }
+        }
+    }
+    auto filts = doc.find("filters");
+    if (filts != nullptr)
+    {
+        auto &filtArray = filts->as<toml::Array>();
+        for (const auto &filt : filtArray)
+        {
+            if (filt.is<toml::Array>())
+            {
+                auto &filtAct = filt.as<toml::Array>();
+                brk->addSourceFilterToEndpoint(filtAct[0].as<std::string>(), filtAct[1].as<std::string>());
+            }
+            else
+            {
+                std::string fname = tomlGetOrDefault(filt, "filter", std::string());
+                if (filt.has("endpoints"))
+                {
+                    auto &targetArray = filt.get<toml::Array>("endpoints");
+                    for (const auto &target : targetArray)
+                    {
+                        brk->addSourceFilterToEndpoint(fname, target.as<std::string>());
+                    }
+                }
+                if (filt.has("source_endpoints"))
+                {
+                    auto &targetArray = filt.get<toml::Array>("source_endpoints");
+                    for (const auto &target : targetArray)
+                    {
+                        brk->addSourceFilterToEndpoint(fname, target.as<std::string>());
+                    }
+                }
+                if (filt.has("dest_endpoints"))
+                {
+                    auto &targetArray = filt.get<toml::Array>("dest_endpoints");
+                    for (const auto &target : targetArray)
+                    {
+                        brk->addDestinationFilterToEndpoint(fname, target.as<std::string>());
                     }
                 }
             }
@@ -79,6 +120,41 @@ void makeConnectionsJson(brkX *brk, const std::string &file)
                     for (const auto &target : conn["targets"])
                     {
                         brk->dataLink(pub, target.asString());
+                    }
+                }
+            }
+        }
+    }
+    if (doc.isMember("filters"))
+    {
+        for (const auto &filt : doc["filters"])
+        {
+            if (filt.isArray())
+            {
+                brk->addSourceFilterToEndpoint(filt[0].asString(), filt[1].asString());
+            }
+            else
+            {
+                std::string fname = jsonGetOrDefault(filt, "filter", std::string());
+                if (filt.isMember("endpoints"))
+                {
+                    for (const auto &target : filt["endpoints"])
+                    {
+                        brk->addSourceFilterToEndpoint(fname, target.asString());
+                    }
+                }
+                if (filt.isMember("source_endpoints"))
+                {
+                    for (const auto &target : filt["source_endpoints"])
+                    {
+                        brk->addSourceFilterToEndpoint(fname, target.asString());
+                    }
+                }
+                if (filt.isMember("dest_endpoints"))
+                {
+                    for (const auto &target : filt["dest_endpoints"])
+                    {
+                        brk->addDestinationFilterToEndpoint(fname, target.asString());
                     }
                 }
             }
