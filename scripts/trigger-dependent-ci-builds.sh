9ea96c4124cd895a94845c92ebee0089f412fa76
Ryan Mast <mast9@llnl.gov>
2018-12-18 16:45:45 -0800

    Trigger HELICS-FMI build on successful HELICS build

--- scripts/trigger-dependent-ci-builds.sh
+++ scripts/trigger-dependent-ci-builds.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 # Trigger HELICS-FMI build
+body='{
+"request": {
+"branch":"master"
+}}'
+
+curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Travis-API-Version: 3" \
+    -H "Authorization: token ${HELICSBOT_TRAVIS_ORG_TOKEN}" \
+    -d "$body" \
+    https://api.travis-ci.org/repo/GMLC-TDC%2FHELICS-FMI/requests
 
 # Trigger helics-ns3 module build
 
