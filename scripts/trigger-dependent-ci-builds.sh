d28b944e66821cebff446f50da15e47a1ec9caaf
Ryan Mast <nightlark@users.noreply.github.com>
2018-12-19 10:56:19 -0800

    CI build triggers (#549)
    * Add a build stage on Travis
    
    * Move some of main Travis config file to separate scripts
    
    * Add a build stage on Travis
    
    * Move some of main Travis config file to separate scripts
    
    * Trigger HELICS-FMI build on successful HELICS build
    
    * Trigger HELICS ns-3 module build on successful build
    
    * Change construction of triggered build message
    
    * Fix JSON syntax for Travis API build request
    
    * Shorten the build message so more shows up on Travis
    
    * Trigger dependent builds only for the develop branch

---
+++ scripts/trigger-dependent-ci-builds.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+BUILD_MESSAGE="Test ${TRAVIS_REPO_SLUG} ${TRAVIS_COMMIT_RANGE}"
+
+# Trigger HELICS-FMI build
+body='{
+"request": {
+"message":"'
+body+="${BUILD_MESSAGE}"
+body+='",
+"branch":"master"
+}}'
+
+curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Travis-API-Version: 3" \
+    -H "Authorization: token ${HELICSBOT_TRAVIS_ORG_TOKEN}" \
+    -d "$body" \
+    https://api.travis-ci.org/repo/GMLC-TDC%2FHELICS-FMI/requests
+
+# Trigger helics-ns3 module build
+body='{
+"definition": {
+"id": 1
+},
+"reason": "individualCI"
+}'
+
+curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Authorization: Basic ${HELICSBOT_AZURE_TOKEN}" \
+    -d "$body" \
+    https://dev.azure.com/HELICS-test/helics-ns3/_apis/build/builds?api-version=4.1
