8ef64a5a5123460428d95a26f0a3da91552afe49
Philip Top <top1@llnl.gov>
2019-01-11 09:09:03 -0800

    Cmake tweaks (#588)
    * Fix FindZeroMQ.cmake to work on windows with the installed version of HELICS
    fix policy to old of swig on cmake newer than 3.13 which changed something in the way swig targets were generated
    
    * wrap the cmake policy in a condition to verify the policy actually exists

--- config/cmake/FindZeroMQ.cmake
+++ config/cmake/FindZeroMQ.cmake
@@ -41,9 +41,11 @@ if (NOT ZeroMQ_LIBRARY_ONLY)
   )
 
   find_path(ZeroMQ_INCLUDE_DIR zmq.h ${ZeroMQ_ROOT_DIR}/include)
-  if (MSVC)
-      # Read registry key holding version
-      get_filename_component(ZeroMQ_NAME "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\ZeroMQ (x64);DisplayVersion]" NAME)
+endif()
+
+ if (MSVC)
+    # Read registry key holding version
+    get_filename_component(ZeroMQ_NAME "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\ZeroMQ (x64);DisplayVersion]" NAME)
 
     # Replace dots with underscores
     string(REGEX REPLACE "\\." "_" ZeroMQ_NAME ${ZeroMQ_NAME})
@@ -69,8 +71,8 @@ if (NOT ZeroMQ_LIBRARY_ONLY)
 		  set(ZeroMQ_DEBUG_LIBRARY_NAME "libzmq-v${CMAKE_VS_PLATFORM_TOOLSET}-mt-gd-${ZeroMQ_NAME}")
 	  endforeach()
     endif()
-  endif()
 endif()
+
 
 find_library(ZeroMQ_LIBRARY
   NAMES
