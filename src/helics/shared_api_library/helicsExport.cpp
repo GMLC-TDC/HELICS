2f3d1030b6ea026a0c3bf70d553ce5a3f4155139
Philip Top <phlptp@gmail.com>
2019-01-10 16:52:18 -0800

    move some of the variable names around to fix a compilation error

--- src/helics/shared_api_library/helicsExport.cpp
+++ src/helics/shared_api_library/helicsExport.cpp
@@ -65,6 +65,8 @@ helics_federate_info helicsCreateFederateInfo ()
     return reinterpret_cast<void *> (fi);
 }
 
+static const char *invalidFedInfoString = "helics Federate info object was not valid";
+
 helics_federate_info helicsFederateInfoClone (helics_federate_info fi, helics_error *err)
 {
     if (fi == nullptr)
@@ -175,8 +177,6 @@ void helicsErrorHandler (helics_error *err) noexcept
 void helicsFederateInfoFree (helics_federate_info fi) { delete reinterpret_cast<helics::FederateInfo *> (fi); }
 
 static const std::string nullstr;
-
-static const char *invalidFedInfoString = "helics Federate info object was not valid";
 
 static helics::FederateInfo *getFedInfo (helics_federate_info fi, helics_error *err)
 {
