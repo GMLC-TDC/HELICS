b4b3424e36e9f261915f5aec9a6aa45312a40428
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2019-02-26 03:15:01 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -72,6 +72,7 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 if read_the_docs_build:
     dir_name = os.path.realpath(os.path.dirname(__file__))
     subprocess.call("cd {dir_name} && make rtddoxygen".format(dir_name=dir_name), shell=True)
+    html_extra_path = [os.path.abspath(os.path.join(dir_name, "../build-doxygen/docs/html"))]
 
 extensions = [
     'sphinx.ext.mathjax',
@@ -217,7 +218,6 @@ man_pages = [(master_doc, 'helics-src', 'HELICS-src Documentation', [author], 1)
 texinfo_documents = [
     (master_doc, 'HELICS-src', 'HELICS-src Documentation', author, 'HELICS-src', 'One line description of project.', 'Miscellaneous'),
 ]
-
 
 def setup(app):
     app.add_stylesheet('css/custom.css')  # may also be an URL
