f4610391c05ecb7d04e9364583d65965f1b9ba44
Philip Top <phlptp@gmail.com>
2019-05-26 16:16:13 -0700

    update copy files scripts and java tests cmake to actually compile

--- config/cmake/copy_key_files.cmake
+++ config/cmake/copy_key_files.cmake
@@ -48,6 +48,56 @@ macro(copy_shared_target target)
   ) # <--this is out- file path
 endmacro()
 
+macro(copy_key_files_to_target_located_at target loc)
+  if(WIN32)
+
+  foreach(keyfile IN LISTS KEY_LIBRARY_FILES)
+    add_custom_command(
+      TARGET
+      ${target}
+      POST_BUILD # Adds a post-build event to api tests
+      COMMAND
+        ${CMAKE_COMMAND}
+        -E
+        copy_if_different # which executes "cmake - E copy_if_different..."
+        "${keyfile}" # <--this is in-file
+        "${loc}/"
+    )
+    # <--this is out-file path
+  endforeach(keyfile)
+  
+   if (TARGET libzmq)
+add_custom_command(
+    TARGET
+     ${target}
+    POST_BUILD # Adds a post-build event to core tests
+    COMMAND
+      ${CMAKE_COMMAND}
+      -E
+      copy_if_different # which executes "cmake - E copy_if_different..."
+      "$<TARGET_FILE:libzmq>" # <--this is in-file
+      "${loc}/"
+  ) # <--this is out- file path
+endif()
+
+endif(WIN32)
+endmacro()
+
+macro(copy_shared_target_at target loc)
+ add_custom_command(
+    TARGET
+    ${target}
+    POST_BUILD # Adds a post-build event to api tests
+    COMMAND
+      ${CMAKE_COMMAND}
+      -E
+      copy_if_different # which executes "cmake - E copy_if_different..."
+      "$<TARGET_FILE:helicsSharedLib>" # <--this is in- file
+      "${loc}/"
+  ) # <--this is out- file path
+endmacro()
+
+
 macro(install_key_files_with_comp comp)
 install(FILES $<TARGET_FILE:helicsSharedLib> DESTINATION ${comp} COMPONENT ${comp})
 install(FILES ${KEY_LIBRARY_FILES} DESTINATION ${comp} COMPONENT ${comp})
