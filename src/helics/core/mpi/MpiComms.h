70d301124d785c6f15a6a87c7af7fed618372097
Philip Top <phlptp@gmail.com>
2019-05-29 07:01:40 -0700

    update header from MpiComms to use container library code

--- src/helics/core/mpi/MpiComms.h
+++ src/helics/core/mpi/MpiComms.h
@@ -7,8 +7,8 @@ SPDX-License-Identifier: BSD-3-Clause
 */
 #pragma once
 
-#include "../../common/BlockingQueue.hpp"
 #include "../CommsInterface.hpp"
+#include "containers/BlockingQueue.hpp"
 #include "helics/helics-config.h"
 #include <atomic>
 #include <future>
