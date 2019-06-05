be6926563d6b6e9b71e93f097a04327e4756da23
Ryan Mast <nightlark@users.noreply.github.com>
2019-06-04 17:15:52 -0700

    Add triggering CI builds in the HELICS-Examples repository (#699)
    * Trigger builds in HELICS-Examples repository for PRs and pushes to develop or master
    
    * Add source branch for HELICS-Examples build

--- scripts/trigger-dependent-ci-builds.sh
+++ scripts/trigger-dependent-ci-builds.sh
@@ -33,3 +33,34 @@ curl -s -X POST \
     -H "Authorization: Basic ${HELICSBOT_AZURE_TOKEN}" \
     -d "$body" \
     https://dev.azure.com/HELICS-test/helics-ns3/_apis/build/builds?api-version=4.1
+
+
+# Trigger HELICS-Examples repository build
+BUILD_PARAMS='\"HELICS_COMMITISH\": \"'$(git rev-parse HEAD)'\"'
+if [[ "${TRAVIS_EVENT_TYPE}" == "push" ]]; then
+    BUILD_PARAMS='\"HELICS_COMMITISH\": \"'${TRAVIS_COMMIT}'\"'
+elif [[ "${TRAVIS_EVENT_TYPE}" == "pull_request" && "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+    BUILD_PARAMS='\"HELICS_COMMITISH\": \"'${TRAVIS_PULL_REQUEST_SHA}'\",'
+    BUILD_PARAMS+='\"HELICS_PR_TARGET\": \"'${TRAVIS_BRANCH}'\",'
+    BUILD_PARAMS+='\"HELICS_PR_NUM\": \"'${TRAVIS_PULL_REQUEST}'\",'
+    BUILD_PARAMS+='\"HELICS_PR_SLUG\": \"'${TRAVIS_REPO_SLUG}'\"'
+fi
+body='{
+"definition": {
+"id": 2
+},
+'
+body+='"parameters": "{'${BUILD_PARAMS}'}",'
+body+='
+"reason": "individualCI",
+"sourceBranch": "refs/heads/HELICS_2_1"
+}'
+
+curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Authorization: Basic ${HELICSBOT_AZURE_TOKEN}" \
+    -d "$body" \
+    https://dev.azure.com/HELICS-test/HELICS-Examples/_apis/build/builds?api-version=4.1
+
+
