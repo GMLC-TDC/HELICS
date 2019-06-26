058cf92cfa3930dd47b8f0476d9a9440b2f23ad8
Philip Top <top1@llnl.gov>
2019-06-25 17:16:08 -0700

    Jsoncpp objlib (#717)
    update the jsoncpp submodule to with objlib capabilities
    
    update the jsoncpp submodule to with objlib capabilities
    
    * Set JsonCpp install includedir to helics/external/optional subfolder
    
    * update to make the default for jsoncpp an objlib everywhere but MSVC
    
    * update jsoncpp objlib submodule pointer
    
    * Apply suggestions from code review
    
    only include jsoncpp_headers for the object library for build
    
    Co-Authored-By: Ryan Mast <nightlark@users.noreply.github.com>
    
    * add an alias target for jsoncpp_headers

--- config/cmake/addJsoncpp.cmake
+++ config/cmake/addJsoncpp.cmake
@@ -8,16 +8,30 @@ set(INSTALL_EXPORT EXPORT helics-targets CACHE INTERNAL "")
 
 set(JSONCPP_DISABLE_CCACHE ON CACHE INTERNAL "")
 
+if (MSVC)
+  option(JSONCPP_OBJLIB OFF "use jsoncpp objlib for linking object files instead of the normal target") 
+else(MSVC)
+ # for Everything but MSVC turn this on to not conflict with system jsoncpp if any
+  option(JSONCPP_OBJLIB ON "use jsoncpp objlib for linking object files instead of the normal target") 
+endif(MSVC)
+
+mark_as_advanced(JSONCPP_OBJLIB)
+
 if(NOT CMAKE_CXX_STANDARD)
     set(CMAKE_CXX_STANDARD 14) # Supported values are ``11``, ``14``, and ``17``.
 endif()
 
 if (BUILD_SHARED_LIBS)
-   set(OLD_BUILD_SHARED_LIBS BUILD_SHARED_LIBS)
+   set(OLD_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
    set(BUILD_SHARED_LIBS OFF)
 endif()
 
-#these are intenal variables used in JSONCPP that we know to be true based on the requirements in HELICS for newer compilers than JSONCPP supports
+if (CMAKE_INSTALL_INCLUDEDIR)
+    set(OLD_CMAKE_INSTALL_INCLUDEDIR ${CMAKE_INSTALL_INCLUDEDIR})
+    set(CMAKE_INSTALL_INCLUDEDIR ${CMAKE_INSTALL_INCLUDEDIR}/helics/external/optional)
+endif()
+
+#these are internal variables used in JSONCPP that we know to be true based on the requirements in HELICS for newer compilers than JSONCPP supports
 set(HAVE_CLOCALE ON)
 set(HAVE_LOCALECONV ON)
 set(COMPILER_HAS_DEPRECATED ON)
@@ -25,8 +39,12 @@ set(HAVE_STDINT_H ON)
 set(HAVE_DECIMAL_POINT ON)
 add_subdirectory("${HELICS_SOURCE_DIR}/ThirdParty/jsoncpp" "${PROJECT_BINARY_DIR}/ThirdParty/jsoncpp")
 
+if (OLD_CMAKE_INSTALL_INCLUDEDIR)
+    set(CMAKE_INSTALL_INCLUDEDIR ${OLD_CMAKE_INSTALL_INCLUDEDIR})
+endif()
+
 if (OLD_BUILD_SHARED_LIBS)
-   set(BUILD_SHARED_LIBS OLD_BUILD_SHARED_LIBS)
+   set(BUILD_SHARED_LIBS ${OLD_BUILD_SHARED_LIBS})
 endif()
 
 mark_as_advanced(
