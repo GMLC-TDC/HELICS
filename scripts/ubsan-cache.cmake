adc627d001394ffa833cf1bd874ab8ddfcafde5b
Philip Top <phlptp@gmail.com>
2019-07-06 06:14:50 -0700

    try using travis build directory

--- scripts/ubsan-cache.cmake
+++ scripts/ubsan-cache.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_C_FLAGS "-fsanitize=undefined -fsanitize-blacklist=.ci/undef_blacklist.txt -g" CACHE STRING "Flags used by the compiler during all build types.")
-set(CMAKE_CXX_FLAGS "-fsanitize=undefined -fsanitize-blacklist=.ci/undef_blacklist.txt -g" CACHE STRING "Flags used by the compiler during all build types.")
-set(CMAKE_EXE_LINKER_FLAGS "-fsanitize=undefined -fsanitize-blacklist=.ci/undef_blacklist.txt" CACHE STRING "Flags used by the linker during all build types.")
-set(CMAKE_MODULE_LINKER_FLAGS "-fsanitize=undefined -fsanitize-blacklist=.ci/undef_blacklist.txt" CACHE STRING "Flags used by the linker during all build types.")
+set(CMAKE_C_FLAGS "-fsanitize=undefined -fsanitize-blacklist=${TRAVIS_BUILD_DIR}/.ci/undef_blacklist.txt -g" CACHE STRING "Flags used by the compiler during all build types.")
+set(CMAKE_CXX_FLAGS "-fsanitize=undefined -fsanitize-blacklist=${TRAVIS_BUILD_DIR}/.ci/undef_blacklist.txt -g" CACHE STRING "Flags used by the compiler during all build types.")
+set(CMAKE_EXE_LINKER_FLAGS "-fsanitize=undefined -fsanitize-blacklist=${TRAVIS_BUILD_DIR}/.ci/undef_blacklist.txt" CACHE STRING "Flags used by the linker during all build types.")
+set(CMAKE_MODULE_LINKER_FLAGS "-fsanitize=undefined -fsanitize-blacklist=${TRAVIS_BUILD_DIR}/.ci/undef_blacklist.txt" CACHE STRING "Flags used by the linker during all build types.")
