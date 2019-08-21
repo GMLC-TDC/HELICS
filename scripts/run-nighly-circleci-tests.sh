becd6a1706ae33e3309d9d88f03b383a6f693da7
Denis Nadeau <dnadeau4@users.noreply.github.com>
2019-08-21 15:16:38 -0700

    add circleci test scripts to cleanup circle.yaml

---
+++ scripts/run-nighly-circleci-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+tests=(
+    /root/project/build/tests/helics/core/core-tests
+    /root/project/build/tests/helics/common/common-tests
+    /root/project/build/tests/helics/system_tests/system-tests
+)
+SUMRESULT=0
+for test in "${tests[@]}"; do
+    echo "${test}"
+    eval "${test}"
+    RESULT=$?
+    echo "***Latest test result: "${RESULT}
+    SUMRESULT=$(( SUMRESULT + RESULT ))
+done
+# Return 0 or a positive integer for failure
+exit ${SUMRESULT}
+
