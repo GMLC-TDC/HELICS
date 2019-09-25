901564d7bedce8177ca670336eeaf28c1c88f285
Philip Top <top1@llnl.gov>
2019-09-25 09:34:33 -0700

    update dockerfile for interface gen and create execution script for doing the generation

---
+++ config/Docker/interface_gen_script.sh
@@ -0,0 +1,12 @@
+
+cd /root/HELICS
+git pull
+
+cd /root/HELICS/build_matlab
+make -j2 mfile_overwrite
+
+cd /root/HELICS/build_interface
+make -j2 pyfile_overwrite
+make -j2 javafile_overwrite
+
+cd /root/HELICS
