77d498a9073d466825c5762d04a1e640bf71ad68
Philip Top <top1@llnl.gov>
2019-09-30 09:35:12 -0700

    add scripts to copy and change the dockerfiles to use ubuntu 19.10 as a base to get clang 9 compilation

---
+++ config/Docker/helics_build_run_msan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git git clone --recurse-submodules --branch develop https://github.com/GMLC-TDC/HELICS.git
+cd HELICS
+mkdir build
+cd build
+cmake .. -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_FLAGS=\"${MSAN_CFLAGS}\"  -DBUILD_HELICS_BOOST_TESTS=OFF -DZMQ_SUBPROJECT=ON  -DZMQ_FORCE_SUBPROJECT=ON
+cd /root/develop/HELICS/build
+make -j2
+
+../scripts/sanitizer_tests.sh
---
+++ scripts/sanitizer_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+tests=(
+    /root/project/build/tests/helics/core/core-tests
+    /root/project/build/tests/helics/common/common-tests
+    "/root/project/build/tests/helics/system_tests/system-tests --gtest_filter=-*realtime*"
+	/root/project/build/tests/helics/apps/helics_apps-tests
+)
+
+SUMRESULT=0
+for test in "${tests[@]}"; do
+    echo "${test}"
+    eval "${test}"
+    RESULT=$?
+    echo "***Latest test result: "${RESULT}
+    SUMRESULT=$(( SUMRESULT + RESULT ))
+done
+# Return 0 or a positive integer for failure
+exit ${SUMRESULT}
+
