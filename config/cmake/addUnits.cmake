119f79ba6e6a2f204e8f963aba9b7aa9536963e5
Philip Top <top1@llnl.gov>
2019-10-24 08:56:17 -0700

    update cmake install paths and units library so it doesn't install

--- config/cmake/addUnits.cmake
+++ config/cmake/addUnits.cmake
@@ -8,22 +8,11 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # so units cpp exports to the correct target export
-set(UNITS_LIBRARY_EXPORT_COMMAND ${HELICS_EXPORT_COMMAND} CACHE INTERNAL "")
-
-set(UNITS_BINARY_ONLY_INSTALL ${HELICS_BINARY_ONLY_INSTALL} CACHE INTERNAL "")
-if( DISABLE_STATIC_LIB_INSTALL)
-    set(UNITS_BINARY_ONLY_INSTALL ON CACHE INTERNAL "")
-endif()
+set(UNITS_INSTALL OFF CACHE INTERNAL "")
 
 if(NOT CMAKE_CXX_STANDARD)
     set(CMAKE_CXX_STANDARD 14) # Supported values are ``11``, ``14``, and ``17``.
 endif()
-
-if(CMAKE_INSTALL_INCLUDEDIR)
-    set(OLD_CMAKE_INSTALL_INCLUDEDIR ${CMAKE_INSTALL_INCLUDEDIR})
-    set(CMAKE_INSTALL_INCLUDEDIR ${CMAKE_INSTALL_INCLUDEDIR}/helics/external/optional)
-endif()
-
 
 set(BUILD_UNITS_OBJECT_LIBRARY OFF CACHE INTERNAL "")
 set(BUILD_UNITS_STATIC_LIBRARY ON CACHE INTERNAL "")
@@ -32,12 +21,8 @@ set(BUILD_UNITS_SHARED_LIBRARY OFF CACHE INTERNAL "")
 add_subdirectory("${HELICS_SOURCE_DIR}/ThirdParty/units"
                  "${PROJECT_BINARY_DIR}/ThirdParty/units")
 
-if(OLD_CMAKE_INSTALL_INCLUDEDIR)
-    set(CMAKE_INSTALL_INCLUDEDIR ${OLD_CMAKE_INSTALL_INCLUDEDIR})
-endif()
-
 set_target_properties(units-static PROPERTIES FOLDER Extern)
-add_library(HELICS::units-static ALIAS units-static)
+add_library(HELICS::units ALIAS units-static)
 
 
 hide_variable(BUILD_UNITS_FUZZ_TARGETS)
