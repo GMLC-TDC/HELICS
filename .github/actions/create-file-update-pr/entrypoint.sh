d948ca880ed74593ab9fcf78e7e0237df58331b3
Ryan Mast <mast9@llnl.gov>
2019-10-25 15:37:42 -0700

    Test getting changed interface files into slightly more generic pr creation action

--- .github/actions/create-file-update-pr/entrypoint.sh
+++ .github/actions/create-file-update-pr/entrypoint.sh
@@ -1,4 +1,10 @@
-#!/bin/sh -l
+#!/bin/bash
 
-uname -a
+files_changed=$(git diff --staged --name-only)
+if [[ "$files_changed" != "" ]];
+then
+  hash=$(sha256sum ${files_changed} | sha256sum | cut -c 1-5 -)
+  echo "ChangeHash=$hash"
+fi
+
 printenv
