e682d1d6d4124116926429c98dcf9198835795ba
Ryan Mast <mast9@llnl.gov>
2019-10-25 15:40:14 -0700

    Extend hash to 12 characters

--- .github/actions/create-file-update-pr/entrypoint.sh
+++ .github/actions/create-file-update-pr/entrypoint.sh
@@ -3,7 +3,7 @@
 files_changed=$(git diff --staged --name-only)
 if [[ "$files_changed" != "" ]];
 then
-  hash=$(sha256sum ${files_changed} | sha256sum | cut -c 1-5 -)
+  hash=$(sha256sum ${files_changed} | sha256sum | cut -c 1-12 -)
   echo "ChangeHash=$hash"
 fi
 
