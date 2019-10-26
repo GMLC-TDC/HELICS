7cbc0057dd25114ee2dff227cad0c7a7853f8dca
Ryan Mast <nightlark@users.noreply.github.com>
2019-10-26 08:21:59 -0700

    Sanity check the hash to make sure it is hashing what we expect

--- .github/actions/create-file-update-pr/entrypoint.sh
+++ .github/actions/create-file-update-pr/entrypoint.sh
@@ -3,8 +3,23 @@
 files_changed=$(git diff --staged --name-only)
 if [[ "$files_changed" != "" ]];
 then
+  sha256sum ${files_changed} > changed_hashes.txt
+  cat changed_hashes.txt
+  hash_chk=$(cat changed_hashes.txt | sha256sum | cut -c 1-12 -)
   hash=$(sha256sum ${files_changed} | sha256sum | cut -c 1-12 -)
-  echo "ChangeHash=$hash"
+  current_branch=$(git branch --show-current)
+  
+  echo "Hash=$hash"
+  echo "Hash Check=$hash_chk"
+  echo "Current branch=$current_branch"
+  
+  git config user.name "${INPUT_GIT_NAME}"
+  git config user.email "${INPUT_GIT_EMAIL}"
+  
+  pr_branch="${INPUT_BRANCH_PREFIX}update-${current_branch}-${hash}"
+  git ls-remote --exit-code . "origin/${pr_branch}"
+  echo $?
+  echo $pr_branch
 fi
 
 cat $GITHUB_EVENT_PATH
