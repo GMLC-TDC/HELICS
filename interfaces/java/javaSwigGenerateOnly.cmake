19621bb9ab642388ee7aa71c44a6f5dbeaebe61b
Ryan Mast <nightlark@users.noreply.github.com>
2019-11-08 16:46:34 -0800

    Add -doxygen flag when updating the Java interface files

--- interfaces/java/javaSwigGenerateOnly.cmake
+++ interfaces/java/javaSwigGenerateOnly.cmake
@@ -11,11 +11,15 @@
 
 file(GLOB SHARED_LIB_HEADERS ${CMAKE_SOURCE_DIR}/src/helics/shared_api_library/*.h)
 
+if(SWIG_VERSION VERSION_GREATER "4.0.0")
+  set(SWIG_DOXYGEN_FLAG "-doxygen")
+endif()
+
   # custom command for building the wrap file
   add_custom_command(
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/helicsJava.c
     COMMAND
-      "${SWIG_EXECUTABLE}" "-java" "-package" "com.java.helics" -o "helicsJava.c"
+      "${SWIG_EXECUTABLE}" "-java" "-package" "com.java.helics" -o "helicsJava.c" "${SWIG_DOXYGEN_FLAG}"
       "-I${CMAKE_SOURCE_DIR}/src/helics/shared_api_library"
       ${CMAKE_CURRENT_SOURCE_DIR}/helicsJava.i
     DEPENDS
