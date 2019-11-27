46d9cebeb680e8451435750b25b82d463dbe066c
Ryan Mast <mast9@llnl.gov>
2019-11-27 10:57:11 -0800

    Add code for getting CPU model name on macOS

--- benchmarks/helics/helics_benchmark_main.h
+++ benchmarks/helics/helics_benchmark_main.h
@@ -90,7 +90,25 @@ inline std::string getCPUIdentifier ()
     }
     return std::string{HELICS_BUILD_PROCESSOR};
 }
-
+#elif defined(__APPLE__)
+#    include <cstdlib>
+#    include <sys/types.h>
+#    include <sys/sysctl.h>
+inline std::string getCPUIdentifier ()
+{
+    std::string info;
+    size_t name_sz = 0;
+    if (!sysctlbyname ("machdep.cpu.brand_string", nullptr, &name_sz, nullptr, 0))
+    {
+        char *buffer = static_cast<char*> (malloc (name_sz));
+        if (!sysctlbyname ("machdep.cpu.brand_string", buffer, &name_sz, nullptr, 0))
+        {
+            info = std::string (buffer, name_sz);
+        }
+        free (buffer);
+    }
+    return info;
+}
 #else
 inline std::string getCPUIdentifier ()
 {  // Get extended ids.
