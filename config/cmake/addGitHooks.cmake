952443d545e8f4e8bee769907bfd42a7fe7aeceb
Mark Eberlein <mark.eberlein@pnnl.gov>
2019-12-02 14:30:41 -0800

    Update GitHooks CMake to remove strict dependencies
    The GitHooks CMake logic will now check for the availability of
    clang-format and for Git version >2.9 automatically, and will
    fail or warn in a reasonable manner depending on detected values.

--- config/cmake/addGitHooks.cmake
+++ config/cmake/addGitHooks.cmake
@@ -1,31 +1,36 @@
 cmake_minimum_required(VERSION 2.8.7)
 
-find_package(Git)
-if(NOT GIT_FOUND)
-    message(FATAL_ERROR "git not found!")
-endif()
-
-include(clang-cxx-dev-tools)
-
 function(set_git_hooks_enabled)
     execute_process(
             COMMAND ${GIT_EXECUTABLE} config --local core.hooksPath .githooks
     )
+    message(STATUS "Git hooks enabled")
 endfunction()
 
 function(set_git_hooks_disabled)
     execute_process(
             COMMAND ${GIT_EXECUTABLE} config --local --unset core.hooksPath
     )
+    message(STATUS "Git hooks disabled")
 endfunction()
 
-option(ENABLE_GIT_HOOKS "Activate git hooks to run clang-format on committed files." ON)
-if (ENABLE_GIT_HOOKS)
-    if (CLANG_FORMAT)
+
+if (NOT DEFINED CLANG_FORMAT)
+    find_program(CLANG_FORMAT "clang-format")
+endif ()
+
+cmake_dependent_advanced_option(HELICS_ENABLE_GIT_HOOKS "Activate git hooks to run clang-format on committed files." ON "CLANG_FORMAT" OFF)
+
+find_package(Git)
+
+if (GIT_FOUND AND (GIT_VERSION_STRING VERSION_GREATER_EQUAL "2.9"))
+    if (HELICS_ENABLE_GIT_HOOKS)
         set_git_hooks_enabled()
-    else()
-        message(FATAL_ERROR "Clang-format is required when ENABLE_GIT_HOOKS is ON. Install clang-format or set ENABLE_GIT_HOOKS to OFF")
-    endif()
+    else ()
+        set_git_hooks_disabled()
+    endif ()
+elseif (GIT_FOUND AND (GIT_VERSION_STRING VERSION_LESS "2.9"))
+    message(WARNING "Git version earlier than 2.9 found, update Git to enable git hooks.")
 else ()
-    set_git_hooks_disabled()
+    message("Git not found, git hooks will not be enabled.")
 endif ()
