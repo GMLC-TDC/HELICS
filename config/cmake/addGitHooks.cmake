d2796ed3db3999ab8ca91d9f7ea6ec4101e76332
Mark Eberlein <mark.eberlein@pnnl.gov>
2019-12-05 14:44:08 -0800

    Add clang-format minimum version checking

--- config/cmake/addGitHooks.cmake
+++ config/cmake/addGitHooks.cmake
@@ -1,4 +1,5 @@
 cmake_minimum_required(VERSION 2.8.7)
+set(CLANG_FORMAT_MIN_VERSION "7.0")
 
 function(set_git_hooks_enabled)
     execute_process(
@@ -16,12 +17,42 @@ endfunction()
 
 
 if (NOT DEFINED CLANG_FORMAT)
-    find_program(CLANG_FORMAT "clang-format")
+    find_program(CLANG_FORMAT clang-format)
 endif ()
+
+if (CLANG_FORMAT)
+    execute_process(COMMAND ${CLANG_FORMAT} -version
+            OUTPUT_VARIABLE CLANG_FORMAT_VERSION
+            ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+endif ()
+
+if (CLANG_FORMAT_VERSION MATCHES "^clang-format version .*")
+    string(REGEX
+            REPLACE "clang-format version ([.0-9]+).*"
+            "\\1"
+            CLANG_FORMAT_VERSION
+            "${CLANG_FORMAT_VERSION}")
+
+    if (NOT CLANG_FORMAT_VERSION VERSION_LESS ${CLANG_FORMAT_MIN_VERSION})
+        set(CLANG_FORMAT_VERSION_OK ON)
+    endif ()
+else ()
+    set(CLANG_FORMAT_VERSION_OK OFF)
+endif ()
+
+mark_as_advanced(CLANG_FORMAT)
 
 cmake_dependent_advanced_option(HELICS_ENABLE_GIT_HOOKS "Activate git hooks to run clang-format on committed files." ON "CLANG_FORMAT" OFF)
 
 find_package(Git)
+
+if (HELICS_ENABLE_GIT_HOOKS AND NOT CLANG_FORMAT_VERSION_OK)
+    message(WARNING
+            "HELICS git hooks require clang-format version ${CLANG_FORMAT_MIN_VERSION} or higher."
+            "\nInstall or update clang-format to use git hooks."
+            "\nTo select a different clang-format exectuable, update the CLANG_FORMAT property in CMake.")
+    SET(HELICS_ENABLE_GIT_HOOKS OFF CACHE BOOL "Activate git hooks to run clang-format on committed files." FORCE)
+endif ()
 
 if (GIT_FOUND AND (NOT GIT_VERSION_STRING VERSION_LESS "2.9"))
     if (HELICS_ENABLE_GIT_HOOKS)
