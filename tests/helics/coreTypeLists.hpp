952b09c89568b23c1b4888d68af95c4358ae845a
Philip Top <top1@llnl.gov>
2019-12-17 12:50:47 -0800

    Apply suggestions from code review
    Co-Authored-By: Ryan Mast <nightlark@users.noreply.github.com>

--- tests/helics/coreTypeLists.hpp
+++ tests/helics/coreTypeLists.hpp
@@ -1,5 +1,5 @@
 /*
-Copyright © 2017-2019,
+Copyright (c) 2017-2019,
 Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance for Sustainable Energy, LLC.  See
 the top-level NOTICE for additional details. All rights reserved.
 SPDX-License-Identifier: BSD-3-Clause
