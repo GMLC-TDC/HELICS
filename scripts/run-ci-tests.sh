24d8d1b31e7cd0c530b7633ef35b45346cf9d427
Philip Top <phlptp@gmail.com>
2020-01-03 08:53:32 -0800

    try adding verbose in a different way

--- scripts/run-ci-tests.sh
+++ scripts/run-ci-tests.sh
@@ -118,7 +118,7 @@ else
     if [[ "$DISABLE_UNIT_TESTS" != "true" ]]; then
 	if [[ -n "${TEST_CONFIG}" ]]; then
         	echo "Running ${TEST_CONFIG} tests"
-        	ctest -L ${TEST_CONFIG} "${CTEST_OPTIONS}"
+        	ctest -L ${TEST_CONFIG} "${CTEST_OPTIONS} --verbose"
 	else
 		echo "Running all tests"
 		ctest "${CTEST_OPTIONS}"
