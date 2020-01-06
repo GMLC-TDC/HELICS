04aef5f1c3aad2e5cea1bbdcf3d570084c898c6e
Philip Top <phlptp@gmail.com>
2020-01-06 06:57:12 -0800

    make a few tweaks in debugging output to discriminate where block occurs

--- src/helics/core/CoreBroker.cpp
+++ src/helics/core/CoreBroker.cpp
@@ -924,10 +924,9 @@ void CoreBroker::processCommand(ActionMessage&& command)
             }
             break;
         case CMD_USER_DISCONNECT:
-            receivedDisconnect1 = true;
             sendDisconnect();
             addActionMessage(CMD_STOP);
-            receivedDisconnect2 = true;
+            receivedDisconnect1 = true;
             break;
         case CMD_DISCONNECT_FED: {
             auto fed = _federates.find(command.source_id);
@@ -943,7 +942,7 @@ void CoreBroker::processCommand(ActionMessage&& command)
             }
         } break;
         case CMD_STOP:
-            receivedDisconnect3 = true;
+            receivedDisconnect2 = true;
             if (!allDisconnected()) { // only send a disconnect message if we haven't done so already
                 timeCoord->disconnect();
                 if (!isRootc) {
@@ -952,6 +951,7 @@ void CoreBroker::processCommand(ActionMessage&& command)
                     transmit(parent_route_id, m);
                 }
             }
+            receivedDisconnect3 = true;
             break;
         case CMD_BROADCAST_DISCONNECT: {
             timeCoord->processTimeMessage(command);
@@ -1719,7 +1719,7 @@ void CoreBroker::disconnect()
                     global_id.load(),
                     getIdentifier(),
                     fmt::format(
-                        "sending disconnect again; total message count = {}, rxdis1={}, rxdis2={} rxdis3",
+                        "sending disconnect again; total message count = {}, rxdis1={}, rxdis2={} rxdis3={}",
                         currentMessageCounter(),
                         receivedDisconnect1.load(),
                         receivedDisconnect2.load(),
