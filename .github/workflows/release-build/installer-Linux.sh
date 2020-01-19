75797efb97b0b8b5508813f0b4b6441406658589
Ryan Mast <nightlark@users.noreply.github.com>
2020-01-19 14:05:21 -0800

    Add a Linux installer to the release builds (#1041)
    * Add a Linux release installer
    
    * Get rid of the shared archive rename for the Linux installer

---
+++ .github/workflows/release-build/installer-Linux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This uses bash variable substitution in a few places
+# 1. getting the cmake directory for running cpack with an absolute path (chocolatey has an unfortunately named alias)
+
+#wget --quiet -O cmake-install.sh https://github.com/Kitware/CMake/releases/download/v3.15.5/cmake-3.15.5-Linux-x86_64.sh && chmod +x cmake-install.sh
+#sudo ./cmake-install.sh --prefix=/usr/local --exclude-subdir --skip-license
+#rm cmake-install.sh
+
+mkdir build && cd build || exit
+cmake -DCMAKE_BUILD_TYPE=Release -DHELICS_ZMQ_SUBPROJECT=ON -DHELICS_ENABLE_PACKAGE_BUILD=ON -DSTATIC_STANDARD_LIB=static -DHELICS_BUILD_EXAMPLES=OFF -DHELICS_BUILD_APP_EXECUTABLES=ON -DHELICS_BUILD_APP_LIBRARY=ON -DBUILD_TESTING=OFF ..
+cmake --build . --config Release
+cpack_dir="$(command -v cmake)"
+cpack_dir="${cpack_dir%/cmake}"
+"${cpack_dir}/cpack" -G "TGZ" -C Release -B "$(pwd)/../artifact"
+cd ../artifact || exit
+rm -rf _CPack_Packages
