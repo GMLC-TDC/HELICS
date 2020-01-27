b20d0c68c91b3eb142b42ff86830f4f6a492f06d
Ryan Mast <nightlark@users.noreply.github.com>
2020-01-27 12:44:01 -0800

    Add man pages for HELICS executables (#1052)
    * Add manpage source files
    
    * Install manpages to share/man subfolder
    
    * Add initial set of generated manpages
    
    * Change manpage file extension from txt to adoc

---
+++ docs/man/generate_manpages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+MAN_DEST_DIR=manpage_out
+mkdir -p "${MAN_DEST_DIR}";
+for filename in helics*.adoc; do
+    a2x --destination-dir="${MAN_DEST_DIR}" --doctype=manpage --format=manpage --no-xmllint "${filename}"
+done
