603598349d8ce527c0da3602c66a74eb686187dc
Philip Top <top1@llnl.gov>
2020-02-03 13:43:35 -0800

    Update the copyright date on the code files to 2020 (#1064)

--- config/cmake/addGitHooks.cmake
+++ config/cmake/addGitHooks.cmake
@@ -1,3 +1,12 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Copyright (c) 2017-2020, Battelle Memorial Institute; Lawrence Livermore
+# National Security, LLC; Alliance for Sustainable Energy, LLC.
+# See the top-level NOTICE for additional details.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 cmake_minimum_required(VERSION 2.8.7)
 set(CLANG_FORMAT_MIN_VERSION "7.0")
 
