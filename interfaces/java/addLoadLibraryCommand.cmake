f5ac8fac171a93be7583e33057f59509863373e5
Philip Top <phlptp@gmail.com>
2020-02-22 14:25:55 -0800

    update the java target

--- interfaces/java/addLoadLibraryCommand.cmake
+++ interfaces/java/addLoadLibraryCommand.cmake
@@ -1,5 +1,5 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Copyright (c) 2017-2019, Battelle Memorial Institute; Lawrence Livermore
+# Copyright (c) 2017-2020, Battelle Memorial Institute; Lawrence Livermore
 # National Security, LLC; Alliance for Sustainable Energy, LLC.
 # See the top-level NOTICE for additional details.
 # All rights reserved.
@@ -8,20 +8,20 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # addLoadLibraryCommand.cmake
-#adds a load library function to the JNIHelics.java
+#adds a load library function to the helicsJava.java
 
 if (NOT LIBRARY_FILE)
-set(LIBRARY_FILE JNIhelics)
+set(LIBRARY_FILE helicsJava)
 else()
 get_filename_component(LIBRARY_FILE ${LIBRARY_FILE} NAME_WE)
 STRING(REGEX REPLACE "^lib" "" LIBRARY_FILE ${LIBRARY_FILE})
 endif()
-file(READ helicsJNI.java HELICS_JNI_SOURCE)
+file(READ helicsJavaJNI.java HELICS_JNI_SOURCE)
 string(FIND "${HELICS_JNI_SOURCE}" "System.loadLibrary" ALREADY_LOADED)
 if (${ALREADY_LOADED} LESS 0)
-string(REPLACE "public class helicsJNI {"
-       "public class helicsJNI {\n  static {\n    System.loadLibrary\(\"${LIBRARY_FILE}\"\);\n  }" HELICS_JNI_SOURCE
+string(REPLACE "public class helicsJavaJNI {"
+       "public class helicsJavaJNI {\n  static {\n    System.loadLibrary\(\"${LIBRARY_FILE}\"\);\n  }" HELICS_JNI_SOURCE
        "${HELICS_JNI_SOURCE}")
    
-file(WRITE helicsJNI.java "${HELICS_JNI_SOURCE}")
+file(WRITE helicsJavaJNI.java "${HELICS_JNI_SOURCE}")
 endif()
