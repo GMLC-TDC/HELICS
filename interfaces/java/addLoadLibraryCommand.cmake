3e73a1f08f4e628f1fb67e8b73fd510e93e306e9
Philip Top <phlptp@gmail.com>
2020-02-22 20:22:20 -0800

    try different addLoadLibraryCommand.cmake

--- interfaces/java/addLoadLibraryCommand.cmake
+++ interfaces/java/addLoadLibraryCommand.cmake
@@ -8,20 +8,20 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # addLoadLibraryCommand.cmake
-#adds a load library function to the helicsJava.java
+#adds a load library function to the helics.java
 
 if (NOT LIBRARY_FILE)
-set(LIBRARY_FILE helicsJava)
+set(LIBRARY_FILE helics)
 else()
 get_filename_component(LIBRARY_FILE ${LIBRARY_FILE} NAME_WE)
 STRING(REGEX REPLACE "^lib" "" LIBRARY_FILE ${LIBRARY_FILE})
 endif()
-file(READ helicsJavaJNI.java HELICS_JNI_SOURCE)
+file(READ helicsJNI.java HELICS_JNI_SOURCE)
 string(FIND "${HELICS_JNI_SOURCE}" "System.loadLibrary" ALREADY_LOADED)
 if (${ALREADY_LOADED} LESS 0)
-string(REPLACE "public class helicsJavaJNI {"
-       "public class helicsJavaJNI {\n  static {\n    System.loadLibrary\(\"${LIBRARY_FILE}\"\);\n  }" HELICS_JNI_SOURCE
+string(REPLACE "public class helicsJNI {"
+       "public class helicsJNI {\n  static {\n    System.loadLibrary\(\"${LIBRARY_FILE}\"\);\n  }" HELICS_JNI_SOURCE
        "${HELICS_JNI_SOURCE}")
    
-file(WRITE helicsJavaJNI.java "${HELICS_JNI_SOURCE}")
+file(WRITE helicsJNI.java "${HELICS_JNI_SOURCE}")
 endif()
