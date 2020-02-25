3a226830e0b96cd06e13fe7488175c41caed6132
Philip Top <top1@llnl.gov>
2020-02-25 12:22:05 -0800

    Cmake new policy (#1093)
    * adjust the cmake policy to actually use the new policy and figure out where the issues with the old policies lie
    
    * tweak some of the python cmake swig stuff to use proper target naming
    
    * try to adjust the java cmake commands and fix compiler warning
    
    * update the java target using alias targets
    
    * try different addLoadLibraryCommand.cmake
    
    * update copyright dates on some files and adjust the CSharp cmake with the updated policy(I don't think it was even available on the old policy)
    
    * update version policy for subdirectory as root cases and some more date updates

--- interfaces/java/addLoadLibraryCommand.cmake
+++ interfaces/java/addLoadLibraryCommand.cmake
@@ -1,5 +1,5 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Copyright (c) 2017-2019, Battelle Memorial Institute; Lawrence Livermore
+# Copyright (c) 2017-2020, Battelle Memorial Institute; Lawrence Livermore
 # National Security, LLC; Alliance for Sustainable Energy, LLC.
 # See the top-level NOTICE for additional details.
 # All rights reserved.
@@ -8,10 +8,10 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # addLoadLibraryCommand.cmake
-#adds a load library function to the JNIHelics.java
+#adds a load library function to the helics.java
 
 if (NOT LIBRARY_FILE)
-set(LIBRARY_FILE JNIhelics)
+set(LIBRARY_FILE helics)
 else()
 get_filename_component(LIBRARY_FILE ${LIBRARY_FILE} NAME_WE)
 STRING(REGEX REPLACE "^lib" "" LIBRARY_FILE ${LIBRARY_FILE})
