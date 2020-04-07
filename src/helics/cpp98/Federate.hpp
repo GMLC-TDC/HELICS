e75a771567ac58f4304becaf59c80976a58f4a4e
Dheepak Krishnamurthy <me@kdheepak.com>
2020-04-07 15:24:24 -0600

    Update c api docstrings (#1173)
    * Update helics.h documentation
    
    * Update api-data.h documentation
    
    * Update helicsCallbacks.h documentation
    
    * Update MessageFederate.h documentation
    
    * Update MessageFilters.h documentation
    
    * Update ValueFederate.h documentation
    
    * Fix documentation that is generated for python
    
    * Apply suggestions from code review
    
    Co-Authored-By: Ryan Mast <nightlark@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: Ryan Mast <nightlark@users.noreply.github.com>
    
    * fix a few warnings and other formatting issues
    
    * Wrap lines with nullptr with forcpponly block
    
    Co-authored-by: Philip Top <top1@llnl.gov>
    Co-authored-by: Ryan Mast <nightlark@users.noreply.github.com>
    Co-authored-by: Philip Top <phlptp@gmail.com>

--- src/helics/cpp98/Federate.hpp
+++ src/helics/cpp98/Federate.hpp
@@ -504,7 +504,7 @@ class Federate {
 
     /** add a dependency for this federate
      @details adds an additional internal time dependency for the federate
-     @param federateName the name of the federate to add a dependency on
+     @param fedName the name of the federate to add a dependency on
      */
     void addDependency(const std::string& fedName)
     {
