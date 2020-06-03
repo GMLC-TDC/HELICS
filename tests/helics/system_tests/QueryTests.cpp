e2ea060504fdb79bc32dd8c5f9168771c0d8b82e
Philip Top <top1@llnl.gov>
2020-06-03 08:43:01 -0700

    Add uuid support in the webserver (#1393)
    * Add uuid support in the webserver
    
    * fix broken test case with missing status query
    
    * Automated formatting of repo files (#1394)
    
    Co-authored-by: Philip Top <top1@llnl.gov>
    Co-authored-by: HELICS-bot <HELICS-bot@users.noreply.github.com>
    
    * update query documentation
    
    * clang tidy fixes
    
    * add another tsan suppression in zmq
    
    * Update docs/user-guide/queries.md
    
    Co-authored-by: Ryan Mast <nightlark@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: HELICS-bot <HELICS-bot@users.noreply.github.com>
    Co-authored-by: Ryan Mast <nightlark@users.noreply.github.com>

--- tests/helics/system_tests/QueryTests.cpp
+++ tests/helics/system_tests/QueryTests.cpp
@@ -6,7 +6,6 @@ SPDX-License-Identifier: BSD-3-Clause
 */
 
 #include "../application_api/testFixtures.hpp"
-#include "gmlc/utilities/stringOps.h"
 #include "helics/application_api/CombinationFederate.hpp"
 #include "helics/application_api/Filters.hpp"
 #include "helics/application_api/Publications.hpp"
@@ -21,7 +20,6 @@ SPDX-License-Identifier: BSD-3-Clause
 struct query: public FederateTestFixture, public ::testing::Test {
 };
 
-using gmlc::utilities::stringOps::removeQuotes;
 class query_type: public ::testing::TestWithParam<const char*>, public FederateTestFixture {
 };
 /** test simple creation and destruction*/
@@ -45,16 +43,16 @@ TEST_P(query_type, publication_queries)
 
     auto core = vFed1->getCorePointer();
     auto res = core->query("fed0", "publications");
-    EXPECT_EQ(res, R"(["pub1","fed0/pub2"])");
+    EXPECT_EQ(res, "[pub1;fed0/pub2]");
     auto rvec = helics::vectorizeQueryResult(res);
 
     ASSERT_EQ(rvec.size(), 2U);
     EXPECT_EQ(rvec[0], "pub1");
     EXPECT_EQ(rvec[1], "fed0/pub2");
-    EXPECT_EQ(vFed2->query("fed0", "publications"), "[\"pub1\",\"fed0/pub2\"]");
+    EXPECT_EQ(vFed2->query("fed0", "publications"), "[pub1;fed0/pub2]");
     EXPECT_EQ(vFed1->query("fed1", "isinit"), "true");
 
-    EXPECT_EQ(vFed1->query("fed1", "publications"), "[\"fed1/pub3\"]");
+    EXPECT_EQ(vFed1->query("fed1", "publications"), "[fed1/pub3]");
     core = nullptr;
     vFed1->finalize();
     vFed2->finalize();
@@ -73,7 +71,7 @@ TEST_P(query_type, broker_queries)
     str.push_back(';');
     str.append(vFed2->getName());
     str.push_back(']');
-    EXPECT_EQ(res, "[\"fed0\",\"fed1\"]");
+    EXPECT_EQ(res, "[fed0;fed1]");
     vFed1->enterInitializingModeAsync();
     vFed2->enterInitializingMode();
     vFed1->enterInitializingModeComplete();
@@ -297,13 +295,13 @@ TEST_F(query, version)
     mFed1->requestTimeAsync(1.0);
     mFed2->requestTime(1.0);
     mFed1->requestTimeComplete();
-    auto res = removeQuotes(mFed1->query("version"));
+    auto res = mFed1->query("version");
     EXPECT_EQ(res, helics::versionString);
-    res = removeQuotes(mFed1->query("broker", "version"));
+    res = mFed1->query("broker", "version");
     EXPECT_EQ(res, helics::versionString);
-    res = removeQuotes(mFed1->query("core", "version"));
+    res = mFed1->query("core", "version");
     EXPECT_EQ(res, helics::versionString);
-    res = removeQuotes(mFed1->query("root", "version"));
+    res = mFed1->query("root", "version");
     EXPECT_EQ(res, helics::versionString);
     res = mFed1->query("root", "version_all");
     auto val = loadJsonStr(res);
@@ -536,7 +534,7 @@ TEST_F(query, updates_indices)
     vFed1->requestTime(1.0);
 
     auto qres = vFed1->query("updated_input_indices");
-    EXPECT_EQ(qres, "[0,1,2]");
+    EXPECT_EQ(qres, "[0;1;2]");
     vFed1->clearUpdates();
     qres = vFed1->query("updated_input_indices");
     EXPECT_EQ(qres, "[]");
@@ -544,7 +542,7 @@ TEST_F(query, updates_indices)
     p3.publish(15.1);
     vFed1->requestTime(2.0);
     qres = vFed1->query("updated_input_indices");
-    EXPECT_EQ(qres, "[0,2]");
+    EXPECT_EQ(qres, "[0;2]");
     vFed1->finalize();
     helics::cleanupHelicsLibrary();
 }
@@ -568,8 +566,7 @@ TEST_F(query, updates_names)
     vFed1->requestTime(1.0);
 
     auto qres = vFed1->query("updated_input_names");
-    auto res = helics::vectorizeQueryResult(qres);
-    EXPECT_EQ(res.size(), 3U);
+    EXPECT_EQ(qres, "[pub1;pub2;pub3]");
     vFed1->clearUpdates();
     qres = vFed1->query("updated_input_names");
     EXPECT_EQ(qres, "[]");
@@ -577,8 +574,7 @@ TEST_F(query, updates_names)
     p3.publish(15.1);
     vFed1->requestTime(2.0);
     qres = vFed1->query("updated_input_names");
-    res = helics::vectorizeQueryResult(qres);
-    EXPECT_EQ(res.size(), 2U);
+    EXPECT_EQ(qres, "[pub1;pub3]");
     vFed1->finalize();
     helics::cleanupHelicsLibrary();
 }
@@ -721,7 +717,7 @@ TEST_F(query, query_subscriptions)
     vFed1->enterInitializingModeComplete();
 
     auto subs = helics::queryFederateSubscriptions(vFed1.get(), "fed1");
-    EXPECT_EQ(subs, "[\"pub1\",\"pub2\",\"pub3\"]");
+    EXPECT_EQ(subs, "[pub1;pub2;pub3]");
     vFed1->finalize();
     vFed2->finalize();
     helics::cleanupHelicsLibrary();
@@ -750,39 +746,21 @@ TEST_F(query, queries_query)
     auto vec = helics::vectorizeQueryResult(res);
     for (auto& qstr : vec) {
         auto qres = vFed1->query(qstr);
-        EXPECT_EQ(qres.find("error"), std::string::npos) << qstr << " produced an error";
-        try {
-            auto v = loadJsonStr(qres);
-        }
-        catch (...) {
-            EXPECT_TRUE(false) << "Unable to load JSON string " << qstr;
-        }
+        EXPECT_NE(qres, "#invalid") << qstr << " produced #invalid";
     }
 
     res = vFed1->query("core", "queries");
     vec = helics::vectorizeQueryResult(res);
     for (auto& qstr : vec) {
         auto qres = vFed1->query("core", qstr);
-        EXPECT_EQ(qres.find("error"), std::string::npos) << qstr << " produced and error in core";
-        try {
-            auto v = loadJsonStr(qres);
-        }
-        catch (...) {
-            EXPECT_TRUE(false) << "Unable to load JSON string " << qstr;
-        }
+        EXPECT_NE(qres, "#invalid") << qstr << " produced #invalid in core";
     }
 
     res = vFed1->query("root", "queries");
     vec = helics::vectorizeQueryResult(res);
     for (auto& qstr : vec) {
         auto qres = vFed1->query("root", qstr);
-        EXPECT_EQ(qres.find("error"), std::string::npos) << qstr << " produced an error in root";
-        try {
-            auto v = loadJsonStr(qres);
-        }
-        catch (...) {
-            EXPECT_TRUE(false) << "Unable to load JSON string " << qstr;
-        }
+        EXPECT_NE(qres, "#invalid") << qstr << " produced #invalid in root";
     }
 
     vFed1->finalize();
