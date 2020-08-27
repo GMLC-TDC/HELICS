f705e6963277c531a84518768050613453c4801e
Nathan <yee29@llnl.gov>
2020-08-27 15:13:19 -0700

    Updating ev federates

--- examples/user_guide_examples/Example_2/merlin_example/uq_example/EVMsgFed.py
+++ examples/user_guide_examples/Example_2/merlin_example/uq_example/EVMsgFed.py
@@ -16,6 +16,7 @@ import numpy as np
 import sys
 import argparse
 
+
 #logging.basicConfig(filename='cosim.log')
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
@@ -31,14 +32,14 @@ def destroy_federate(fed):
     h.helicsCloseLibrary()
     #print("EVController: Federate finalized")
 
-def create_message_federate(fedinitstring,name,deltat):
+def create_message_federate(fedinitstring,name,period):
     fedinfo = h.helicsCreateFederateInfo()
     h.helicsFederateInfoSetCoreTypeFromString(fedinfo, "tcpss")
     h.helicsFederateInfoSetCoreInitString(fedinfo, fedinitstring)
     # Set one second message interval
-    h.helicsFederateInfoSetTimeProperty(fedinfo, h.helics_property_time_delta, deltat)
+    h.helicsFederateInfoSetTimeProperty(fedinfo, h.helics_property_time_period, period)
     # set wait for current time update to true
-    h.helicsFederateInfoSetFlagOption(fedinfo, h.helics_flag_wait_for_current_time_update, False)
+    h.helicsFederateInfoSetFlagOption(fedinfo, h.helics_flag_uninterruptible, True)
     h.helicsFederateInfoSetIntegerProperty(fedinfo, h.helics_property_int_log_level, 1)
     fed = h.helicsCreateMessageFederate(name, fedinfo)
     #print("Message federate created")
@@ -48,11 +49,9 @@ def create_message_federate(fedinitstring,name,deltat):
 def get_new_EV(numEVs):
     # numEVs is the number of EVs to return to the main program
 
-    print("Getting New EV")
-
-    lvl2 = np.random.lognormal(np.random.normal(5,1),np.random.uniform(0,2),1)
-    lvl1 = np.random.lognormal(np.random.normal(3,1),np.random.uniform(0,10),1)
-    lvl3 = np.random.lognormal(np.random.normal(2,1),np.random.uniform(0,.1),1)
+    lvl1 = np.random.poisson(np.random.normal(30,np.random.uniform(1,3)),1)
+    lvl2 = np.random.poisson(np.random.normal(50,np.random.uniform(1,2)),1)
+    lvl3 = np.random.poisson(np.random.normal(20,np.random.uniform(.05,.25)),1)
     total = lvl1+lvl2+lvl3
     #print(lvl1,lvl2,lvl3,total)
     p1,p2,p3 = lvl1/total,lvl2/total,lvl3/total
@@ -83,9 +82,12 @@ if __name__ == "__main__":
 
     print("Federate Init String = {}".format(fedinitstring)) 
     print("Random seed = {}".format(args.seed))
+
+
+
     name = 'EV_federate'
-    deltat = 60*60  # this is the default time step for the EV federate
-    fed = create_message_federate(fedinitstring,name,deltat)
+    period = 15*60  # 15 min  # this is the default time step for the EV federate
+    fed = create_message_federate(fedinitstring,name,period)
 
     #### Register interfaces #####
 # Register the endpoints and their destinations
@@ -116,8 +118,7 @@ if __name__ == "__main__":
 
     fed_name = h.helicsFederateGetName(fed)
     #print(" Federate {} has been registered".format(fed_name))
-    test = h.helicsEndpointGetName(end_EVsoc[0])
-    #print(test)
+
 #
 ######################   Entering Execution Mode  ##########################################################
 
@@ -153,129 +154,88 @@ if __name__ == "__main__":
     # lvl2 = leaf, 240V
     # lvl3 = leaf, 480V
     # assumes 15amp outlet, same battery size
-    charge_rate = [1.8,3.6,7.2]
-    batt_size = 62*60 # leaf capacity is 62 kWh
-    hours = 24*7
+    charge_rate = [1.8,7.2,50]
+    # [120V*15A, 240V*30A, 50kW DC charging]
+    batt_size = 62 # leaf capacity is 62 kWh
+    hours = 24*7 # one week
     total_interval = int(60 * 60 * hours)
-    update_interval = 60*60 # updates every hour
+    update_interval = 30*60 # updates every hour
     grantedtime = -1
 
     numLvl1,numLvl2,numLvl3,EVlist = get_new_EV(end_count)
 ##
 ## Step through each time period starting from t = 0
     time_sim = []; currentsoc = np.random.rand(end_count)
-    send_soc = 1; t = 0
+    #t = 0
+
+    # this will be the first message that is "waiting" for the
+    # EV Controller
+
+    t = h.helicsFederateRequestTime (fed, 0)
+    for j in range(0,len(enddest_EVsoc)):
+        end_name = str(h.helicsEndpointGetName(end_EVsoc[j]))
+        destination_name = str(h.helicsEndpointGetDefaultDestination(end_EVsoc[j]))
+        h.helicsEndpointSendMessageRaw(end_EVsoc[j], "", str(currentsoc[j])) #
+        #print('destination: ',destination_name)
+        #print('message sent: ',str(currentsoc[j]))
+        #print('at time: ',t/3600)
+        #print(t/3600,currentsoc[j])
+
+    grantedtime = h.helicsFederateRequestTime (fed, (t+update_interval))
+    t = grantedtime
+
+    #logger.info('time: ',t)
     time_since_last_msg = np.zeros(end_count)
-    print('time,power')
+    print('time power')
+    next_time = []
     while t < total_interval:
+
+
+
+        for j in range(0,len(enddest_EVsoc)):
+            # 1. Receive instructions
+            if h.helicsEndpointHasMessage(end_EVsoc[j]):
+                msg = h.helicsEndpointGetMessageObject(end_EVsoc[j])
+                instructions = h.helicsMessageGetString(msg)
+            # 2. Change SOC based on instructions
+                if int(instructions) == 1:
+                    addenergy = charge_rate[(EVlist[j]-1)]*(update_interval/3600)   #time_since_last_msg[j]
+                    #print('old soc: ',currentsoc[j])
+                    currentsoc[j] = currentsoc[j] + addenergy/batt_size
+                    #print('new soc: ',currentsoc[j])
+                else:
+                    _,_,_,newEVtype = get_new_EV(1)
+                    EVlist[j] = newEVtype[0]
+                    currentsoc[j] = np.random.uniform(.05,.5)
+                    #print('new soc: ',currentsoc[j])
+                # 3. Send SOC
+                #end_name = str(h.helicsEndpointGetName(end_EVsoc[j]))
+                destination_name = str(h.helicsEndpointGetDefaultDestination(end_EVsoc[j]))
+                h.helicsEndpointSendMessageRaw(end_EVsoc[j], "", str(currentsoc[j])) #
+                #print(t/3600,currentsoc[j])
+            else:
+                #currentsoc[j] = np.random.rand(end_count)
+                #print('new value: ',currentsoc[j])
+                print('error AT TIME ',t,' with endpoint ',str(h.helicsEndpointGetDefaultDestination(end_EVsoc[j])))
+                #print('maybe we need to advance in time by this federates time delta, ',h.helicsFederateRequestTime (fed, t))
 
         total_power = 0
         for j in range(0,len(enddest_EVsoc)):
             total_power += charge_rate[(EVlist[j]-1)]
-
         print(t/3600,total_power)
-        # print("Requesting time ", t)
-        grantedtime = h.helicsFederateRequestTime (fed, t)
-        # print('NEXT TIME WILL BE: ',grantedtime/3600)
-
-        # current minute
-        time_sim.append(t/3600)
-        #print('CURRENT TIME in HOURS: ',t/3600)
-
-        if send_soc:
-            # First send SOC from each EV in the fleet to the EVController
-            for j in range(0,len(enddest_EVsoc)):
-                #print('sending soc to controller: ',currentsoc[j])
-                # log the source of the message
-                end_name = str(h.helicsEndpointGetName(end_EVsoc[j]))
-                # this is the name of the source endpoint
-                #print('Sending endpoint name: ',end_name)
-                destination_name = str(h.helicsEndpointGetDefaultDestination(end_EVsoc[j]))
-                #print('destination endpoint name: ',destination_name)
-                #destination_federate = str(destination_name.split('/EV')[0])
-                #print('Endpoint destination federate: ',destination_federate)
-                h.helicsEndpointSendMessageRaw(end_EVsoc[j], "", str(currentsoc[j])) #
-
-                #print('Sent SOC: {}'.format(currentsoc[j]))
-
-                #print('')
-            # after sending the soc of each EV,
-            send_soc = 0
-            # wait 1 min for instructions
-            t = grantedtime
-        else:
-            # ask for instructions
-            instructions = []
-            for j in range(0,len(enddest_EVsoc)):
-                # log the source of the message
-                #print('endpt name: ',h.helicsEndpointGetName(end_EVsoc[j]))
-
-                if h.helicsEndpointHasMessage(end_EVsoc[j]):
-                    msg = h.helicsEndpointGetMessageObject(end_EVsoc[j])
-                    #print('MESSAGE RECEIVED: ',h.helicsMessageGetString(msg))
-                    instructions.append(h.helicsMessageGetString(msg))
-                    #print('msg: ',instructions[j])
-                    # right now assume
-                    if int(instructions[j]) == 1:
-                        # add  kW times time difference
-                        # EVlist[j] = 1, 2, or 3
-                        # subtract 1 to get the index for the
-                        # charge rate of that level EV
-                        #print('OLD SOC: ',currentsoc[j])
-                        #print('TIME DIFF IN HOURS: ',time_since_last_msg[j])
-                        #print('EVLIST: ',EVlist[j])
-                        #print('INDEX: ',(EVlist[j]-1))
-                        # add e.g., 1.9 kW for the past minute of charging
-                        addenergy = charge_rate[(EVlist[j]-1)]*time_since_last_msg[j]
-                        #    # convert to %
-                        #print('              ADDING THIS AMOUNT OF ENERGY: ',addenergy)
-                        #print('              AS PERCENT OF BATT SIZE: ',addenergy/batt_size)
-                        currentsoc[j] = currentsoc[j] + addenergy/batt_size
-                        #print('NEW SOC: ',currentsoc[j])
-                    else:
-                        # this EV has enough energy stored
-                        # the controller has "disengaged" the EV,
-                        # and the EV federation should select a new random
-                        # EV
-                        # reassign the j-th EV this new information
-                        #print('WAS A ',EVlist[j])
-                        _,_,_,newEVtype = get_new_EV(1)
-                        EVlist[j] = newEVtype[0]
-                        #print('CONGRATS, ITS A ',EVlist[j])
-                        currentsoc[j] = np.random.uniform(.05,.5)
-                        #print('SOC: ',currentsoc[j])
-                    send_soc = 1
-                    time_since_last_msg[j] = 0
-                else:
-                    #print('NO MESSAGE AT TIME ',t/3600)
-                    time_since_last_msg[j] += 1
-                    #print('TIME SINCE LAST MSG: ',time_since_last_msg[j])
-                #if h.helicsEndpointHasMessage(msg):
-                #    instructions.append(
-                #        h.helicsMessageGetString(msg)
-                #    )
-                #    print('msg: ',instructions[j])
-                #    # right now assume
-                #    if instructions[j] == 1:
-                #        # add  kW times time difference
-                #        # EVlist[j] = 1, 2, or 3
-                #        # subtract 1 to get the index for the
-                #        # charge rate of that level EV
-                #        addenergy = charge_rate[(EVlist[j]-1)]*(time_sim[-1]-time_sim[-2])
-                #    #    # convert to %
-                #        currentsoc[j] = currentsoc[j] + addenergy/batt_size
-                #    else:
-                #        # this EV has enough energy stored
-                #        # the controller has "disengaged" the EV,
-                #        # and the EV federation should select a new random
-                #        # EV
-                #        # reassign the j-th EV this new information
-                #        _,_,_,EVlist[j] = get_new_EV(1)
 
 
 
-            t = grantedtime
+        grantedtime = h.helicsFederateRequestTime (fed, (t+update_interval))
+        #print('EV time: ',grantedtime/3600)
+
+        t = grantedtime
+
+
+
+
+
 
     #print("Destroying federate")
     destroy_federate(fed)
-    print("Done!")
+    #print("Done!")
