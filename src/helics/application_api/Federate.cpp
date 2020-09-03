9a1dd054e439841e92a35fe88def011bf14bb79a
Trevor Hardy <trevor.hardy@pnnl.gov>
2020-09-03 13:32:31 -0700

    Normalize source_targets
    Standardized names:
    	source_targets
    	sourcetargets
    	sourceTargets
    
    Deprecated names:
    	targets

--- src/helics/application_api/Federate.cpp
+++ src/helics/application_api/Federate.cpp
@@ -806,11 +806,11 @@ static void loadOptions(const Inp& data, Filter& filt)
     }
     auto asrc = [&filt](const std::string& target) { filt.addSourceTarget(target); };
     auto adest = [&filt](const std::string& target) { filt.addDestinationTarget(target); };
-    addTargets(data, "targets", asrc);
     addTargets(data, "sourcetargets", asrc);
-    addTargets(data, "desttargets", adest);
     addTargets(data, "sourceTargets", asrc);
+    addTargets(data, "source_targets", asrc);
     addTargets(data, "destTargets", adest);
+    addTargets(data, "desttargets", adest);
 }
 
 void Federate::registerFilterInterfacesJson(const std::string& jsonString)
