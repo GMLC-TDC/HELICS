fc120d50abfd68adf2a6ba2f9d09536482e791ed
Ryan Mast <nightlark@users.noreply.github.com>
2020-10-09 11:10:39 -0700

    Bump macos sdk install script to add 10.14 sdk

--- .github/actions/setup-macos-10.9-sdk.sh
+++ .github/actions/setup-macos-10.9-sdk.sh
@@ -3,7 +3,7 @@
 XCODE_MACOS_PLATFORM_PATH="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform"
 XCODE_MACOS_SDK_PATH="${XCODE_MACOS_PLATFORM_PATH}/Developer/SDKs"
 
-SDK_VER=10.9
+SDK_VER=10.14
 
 # Download macOS SDK
 wget "https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX${SDK_VER}.sdk.tar.xz"
