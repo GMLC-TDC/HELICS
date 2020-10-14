e21ad6514e8479a234056d029c7e5753581cdbc2
Philip Top <top1@llnl.gov>
2020-10-14 09:25:16 -0700

    Get flag enhancement (#1655)
    * add indexing tests and missing ones for the flag enumerations
    
    * Automated formatting of repo files (#1646)
    
    * C api query callback (#1634)
    
    * Add a query callback for the C api
    
    * add queryCallback to the C api including some additional tests
    
    * Automated formatting of repo files (#1635)
    
    Co-authored-by: Philip Top <top1@llnl.gov>
    Co-authored-by: HELICS-bot <HELICS-bot@users.noreply.github.com>
    
    * Fix static analyzer issues
    
    * Automated formatting of repo files (#1636)
    
    Co-authored-by: Philip Top <top1@llnl.gov>
    Co-authored-by: HELICS-bot <HELICS-bot@users.noreply.github.com>
    
    * fix seg fault
    
    * Automated formatting of repo files (#1637)
    
    Co-authored-by: Philip Top <top1@llnl.gov>
    Co-authored-by: HELICS-bot <HELICS-bot@users.noreply.github.com>
    
    * Fix some codacy and asan errors in the queryTests
    
    * update comment spacing
    
    * Automated formatting of repo files (#1638)
    
    Co-authored-by: Philip Top <top1@llnl.gov>
    Co-authored-by: HELICS-bot <HELICS-bot@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Ryan Mast <nightlark@users.noreply.github.com>
    
    * Update Federate.hpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: HELICS-bot <HELICS-bot@users.noreply.github.com>
    Co-authored-by: Ryan Mast <nightlark@users.noreply.github.com>
    
    * Automated formatting of repo files
    
    Co-authored-by: Philip Top <top1@llnl.gov>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: HELICS-bot <HELICS-bot@users.noreply.github.com>
    Co-authored-by: Ryan Mast <nightlark@users.noreply.github.com>
    
    * fix tests
    
    * allow some additional flag values to be retrieved from the core
    
    * Automated formatting of repo files (#1656)
    
    Co-authored-by: Philip Top <top1@llnl.gov>
    Co-authored-by: HELICS-bot <HELICS-bot@users.noreply.github.com>
    
    * clang tidy issues
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: HELICS-bot <HELICS-bot@users.noreply.github.com>
    Co-authored-by: Ryan Mast <nightlark@users.noreply.github.com>

--- tests/helics/core/CoreConfigureTests.cpp
+++ tests/helics/core/CoreConfigureTests.cpp
@@ -38,3 +38,17 @@ TEST(CoreConfig, test3)
     EXPECT_EQ(cr->getIdentifier(), "core_name10");
     cr->disconnect();
 }
+
+TEST(CoreConfig, getFlagTests)
+{
+    auto cr =
+        helics::CoreFactory::create(helics::core_type::TEST, "--force_logging_flush --dumplog");
+    cr->setFlagOption(helics::local_core_id, helics_flag_delay_init_entry, true);
+
+    EXPECT_TRUE(cr->getFlagOption(helics::local_core_id, helics_flag_delay_init_entry));
+    EXPECT_FALSE(cr->getFlagOption(helics::local_core_id, helics_flag_enable_init_entry));
+
+    EXPECT_TRUE(cr->getFlagOption(helics::local_core_id, helics_flag_force_logging_flush));
+    EXPECT_TRUE(cr->getFlagOption(helics::local_core_id, helics_flag_dumplog));
+    cr->disconnect();
+}
