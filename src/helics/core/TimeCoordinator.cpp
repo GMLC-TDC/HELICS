975d49627d3aeae47cde9ea131df69f08913431a
Philip Top <phlptp@gmail.com>
2021-03-30 12:43:21 -0700

    fix time coordinator undefined behavior possibilities

--- src/helics/core/TimeCoordinator.cpp
+++ src/helics/core/TimeCoordinator.cpp
@@ -190,9 +190,17 @@ bool TimeCoordinator::updateNextExecutionTime()
             time_exec = (iterating == iteration_request::no_iterations) ? getNextPossibleTime() :
                                                                           time_granted;
         }
-        if ((time_exec - time_granted) > timeZero) {
+        if (time_granted < Time::maxVal())
+        {
+            if ((time_exec - time_granted) > timeZero) {
+                time_exec = generateAllowedTime(time_exec);
+            }
+        }
+        else
+        {
             time_exec = generateAllowedTime(time_exec);
         }
+        
     }
 
     return (time_exec != cexec);
@@ -349,6 +357,10 @@ Time TimeCoordinator::getNextPossibleTime() const
             retTime += info.period;
         }
         return retTime;
+    }
+    if (time_grantBase >= Time::maxVal() - std::max(info.timeDelta, info.period))
+    {
+        return Time::maxVal();
     }
     return generateAllowedTime(time_grantBase + std::max(info.timeDelta, info.period));
 }
