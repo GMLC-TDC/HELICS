ec1a6cab1081a4ad431e9e358ae30b5852127294
Philip Top <phlptp@gmail.com>
2021-04-27 12:39:37 -0700

    update some docs and 2021 dates

--- scripts/_git-all-archive.sh
+++ scripts/_git-all-archive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2017-2019,
+# Copyright (c) 2017-2021,
 # Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance for Sustainable Energy, LLC.  See
 # the top-level NOTICE for additional details. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
