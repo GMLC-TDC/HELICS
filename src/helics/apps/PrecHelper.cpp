e0eea6d6e3de2fb67870f3dfb623410f7f385aea
Philip Top <top1@llnl.gov>
2021-05-26 13:27:53 -0700

    Fix an issue with the recorder capture text file  (#1910)
    * Fix an issue with the recorder capture text file storing the type in the wrong location which could cause issues on reading the file in a player
    
    * Automated formatting of repo files (#1911)
    
    Co-authored-by: Philip Top <top1@llnl.gov>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * clang-tidy fixes
    
    * use Json string converters instead of a custom escaping function
    
    * Automated formatting of repo files (#1918)
    
    Co-authored-by: Philip Top <top1@llnl.gov>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update docs/apps/Player.md
    
    Co-authored-by: Ryan Mast <nightlark@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Ryan Mast <nightlark@users.noreply.github.com>

--- src/helics/apps/PrecHelper.cpp
+++ src/helics/apps/PrecHelper.cpp
@@ -80,3 +80,10 @@ bool isBinaryData(helics::SmallBuffer& data)
         return ((c < 32) || (c == 34) || (c > 126));
     });
 }
+
+bool isEscapableData(helics::data_block& data)
+{
+    return std::all_of(data.begin(), data.end(), [](const auto& c) {
+        return ((c >= 32 && c <= 126) || (c == '\t') || (c == '\n'));
+    });
+}
