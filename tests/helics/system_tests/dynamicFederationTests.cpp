d424b4761cac9142087558e887bd22af372741d7
Philip Top <top1@llnl.gov>
2021-09-28 16:24:01 -0700

    a test with a subscription for observerFeds

--- tests/helics/system_tests/dynamicFederationTests.cpp
+++ tests/helics/system_tests/dynamicFederationTests.cpp
@@ -33,7 +33,7 @@ TEST_F(dynFed, simple_observer)
     vFed2->setProperty(HELICS_PROPERTY_TIME_PERIOD, 0.5);
     auto& pub = vFed1->registerGlobalPublication<double>("pub1");
     vFed2->registerSubscription("pub1");
-    vFed1->enterExecutingModeAsync();
+    vFed1->enterExecutingModeAsync(); 
     vFed2->enterExecutingMode();
     vFed1->enterExecutingModeComplete();
     pub.publish(0.27);
@@ -73,6 +73,64 @@ TEST_F(dynFed, simple_observer)
 
     cdyn->disconnect();
 
+
+    // now we try to join the federation with an observer
+    vFed1->finalize();
+    vFed2->finalize();
+}
+
+
+
+/** just a check that in the simple case we do actually get the time back we requested*/
+TEST_F(dynFed, observer_subscriber)
+{
+    SetupTest<helics::ValueFederate>("test_2", 2);
+    auto vFed1 = GetFederateAs<helics::ValueFederate>(0);
+    auto vFed2 = GetFederateAs<helics::ValueFederate>(1);
+
+    vFed1->setProperty(HELICS_PROPERTY_TIME_PERIOD, 0.5);
+    vFed2->setProperty(HELICS_PROPERTY_TIME_PERIOD, 0.5);
+    auto& pub = vFed1->registerGlobalPublication<double>("pub1");
+    vFed2->registerSubscription("pub1");
+    vFed1->enterExecutingModeAsync();
+    vFed2->enterExecutingMode();
+    vFed1->enterExecutingModeComplete();
+    pub.publish(0.27);
+    auto res = vFed1->requestTime(2.0);
+    EXPECT_EQ(res, 2.0);
+
+    res = vFed2->requestTime(2.0);
+    EXPECT_EQ(res, 0.5);  // the result should show up at the next available time point
+    res = vFed2->requestTime(2.0);
+    EXPECT_EQ(res, 2.0);
+
+    helics::FederateInfo fi(helics::CoreType::TEST);
+    fi.observer = true;
+
+    auto bname = brokers[0]->getIdentifier();
+    auto cdyn = helics::CoreFactory::create(helics::CoreType::TEST,
+                                            "coredyn",
+                                            std::string("--observer --broker=") + bname);
+
+    EXPECT_TRUE(cdyn->connect());
+    fi.coreName = "coredyn";
+
+    std::shared_ptr<helics::ValueFederate> fobs;
+    EXPECT_NO_THROW(fobs = std::make_shared<helics::ValueFederate>("fedObs", fi));
+
+    if (fobs) {
+        auto obsSubs = fobs->registerSubscription("pub1");
+        fobs->query("root", "global_flush");
+
+        EXPECT_NO_THROW(fobs->enterInitializingMode());
+
+        EXPECT_NO_THROW(fobs->enterExecutingMode());
+
+        EXPECT_EQ(fobs->getCurrentTime(), helics::timeZero);
+        fobs->disconnect();
+    }
+
+    cdyn->disconnect();
 
     // now we try to join the federation with an observer
     vFed1->finalize();
