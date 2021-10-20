de3ef5d57981b115654129578114728008336d30
Ryan Mast <mast9@llnl.gov>
2021-10-19 17:45:41 -0700

    Fix building IPC core with Boost 1.77.0

--- src/helics/network/ipc/IpcQueueHelper.cpp
+++ src/helics/network/ipc/IpcQueueHelper.cpp
@@ -8,6 +8,7 @@ SPDX-License-Identifier: BSD-3-Clause
 #include "IpcQueueHelper.h"
 
 #include <boost/date_time/posix_time/ptime.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <string>
 #include <thread>
 
@@ -115,7 +116,11 @@ namespace ipc {
         while (true) {
             if (timeout >= 0) {
                 boost::posix_time::ptime abs_time =
+#if BOOST_VERSION >= 107700
+                    boost::interprocess::ipcdetail::microsec_clock<boost::posix_time::ptime>::universal_time();
+#else
                     boost::date_time::microsec_clock<boost::posix_time::ptime>::universal_time();
+#endif
                 abs_time += boost::posix_time::milliseconds(timeout);
                 bool res =
                     rqueue->timed_receive(buffer.data(), mxSize, rx_size, priority, abs_time);
