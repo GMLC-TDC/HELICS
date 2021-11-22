be5d3c666e274778c61d9c582c55614e86a72431
Philip Top <phlptp@gmail.com>
2021-11-22 07:59:03 -0800

    try adding a disconnect call in one of the tests

--- tests/helics/application_api/CombinationFederateTests.cpp
+++ tests/helics/application_api/CombinationFederateTests.cpp
@@ -108,6 +108,7 @@ TEST_P(combofed_single_type_tests, single_transfer)
     s = subid.getString();
 
     EXPECT_EQ(s, "string2");
+    vFed1->disconnect();
 }
 
 TEST_P(combofed_single_type_tests, endpoint_registration)
