c1349ee043c677519432d6ac91f1c3161f6b4bc0
Philip Top <top1@llnl.gov>
2021-12-28 15:13:54 -0800

    clean up the usage of the network default ports  (#2243)
    * clean up the usage of the network default ports to make it easier if we decide to use an environment variable later
    
    * fix static analysis warnings
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * change the default name
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/helics/network/networkDefaults.cpp
@@ -0,0 +1,33 @@
+/*
+Copyright (c) 2017-2021,
+Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance for Sustainable
+Energy, LLC.  See the top-level NOTICE for additional details. All rights reserved.
+SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#include "networkDefaults.hpp"
+
+#include "../helics_enums.h"
+
+namespace helics {
+int getDefaultPort(int coreType)
+{
+    switch (coreType) {
+        case HELICS_CORE_TYPE_TCP:
+            return network::DEFAULT_TCP_PORT;
+        case HELICS_CORE_TYPE_TCP_SS:
+            return network::DEFAULT_TCPSS_PORT;
+        case HELICS_CORE_TYPE_ZMQ:
+            return network::DEFAULT_ZMQ_PORT;
+        case HELICS_CORE_TYPE_ZMQ_SS:
+            return network::DEFAULT_ZMQSS_PORT;
+        case HELICS_CORE_TYPE_UDP:
+            return network::DEFAULT_UDP_PORT;
+        case HELICS_CORE_TYPE_HTTP:
+        case HELICS_CORE_TYPE_WEBSOCKET:
+            return 80;
+        default:
+            return (-1);
+    }
+}
+}  // namespace helics
