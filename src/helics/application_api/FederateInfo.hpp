873c63e3cf878163b037bf0f58ea5218ebe19f51
Philip Top <top1@llnl.gov>
2022-02-25 19:03:20 -0800

    Abort message change (#2287)
    * Change the abort message and add and error code and refactor the way the apps were handing the federateInfo arguments
    
    * remove redundant help call
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix namespace
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Apply suggestions from code review
    
    Co-authored-by: Ryan Mast <3969255+nightlark@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Ryan Mast <3969255+nightlark@users.noreply.github.com>
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Ryan Mast <3969255+nightlark@users.noreply.github.com>

--- src/helics/application_api/FederateInfo.hpp
+++ src/helics/application_api/FederateInfo.hpp
@@ -13,6 +13,10 @@ SPDX-License-Identifier: BSD-3-Clause
 #include <string>
 #include <vector>
 
+namespace CLI {
+class App;
+}
+
 namespace helics {
 class helicsCLI11App;
 /** data class defining federate properties and information
@@ -108,6 +112,9 @@ class HELICS_CXX_EXPORT FederateInfo: public CoreFederateInfo {
     Time checkTimeProperty(int propId, Time defVal) const;
     bool checkFlagProperty(int propId, bool defVal) const;
     int checkIntProperty(int propId, int defVal) const;
+
+    /** inject the federateInfo parser into another CLI11 App*/
+    void injectParser(CLI::App* app);
 
   private:
     std::unique_ptr<helicsCLI11App> makeCLIApp();
