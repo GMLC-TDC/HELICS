d000442de51ba35cb0a81d0088398d60bbe6405f
Ryan Mast <3969255+nightlark@users.noreply.github.com>
2022-03-05 08:29:36 -0800

    Add encryption related CI tests (#2304)
    * Add encryption related CI tests
    
    * Add files for encryption tests
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/helics/system_tests/networkTests.cpp
+++ tests/helics/system_tests/networkTests.cpp
@@ -6,10 +6,12 @@ SPDX-License-Identifier: BSD-3-Clause
 */
 
 #include "../application_api/testFixtures.hpp"
+#include "../apps/exeTestHelper.h"
 #include "helics/ValueFederates.hpp"
 #include "helics/helics-config.h"
 
 #include "gtest/gtest.h"
+#include <thread>
 
 /** tests for some network options*/
 
@@ -255,4 +257,106 @@ TEST_F(network_tests, test_core_type_env)
         broker->disconnect();
     }
 }
+#endif
+
+#ifdef HELICS_ENABLE_ENCRYPTION
+#    ifdef HELICS_ENABLE_TCP_CORE
+TEST_F(network_tests, test_encrypted_tcp)
+{
+    const std::string brokerArgs =
+        "-f 2 --core_type=tcp --encrypted --encryption_config=" + std::string(TEST_BIN_DIR) +
+        "encryption_config/openssl.json";
+    exeTestRunner brokerExe(HELICS_BROKER_LOCATION, "helics_broker");
+    if (!brokerExe.isActive()) {
+        std::cout << " unable to locate helics_broker in " << HELICS_BROKER_LOCATION << std::endl;
+    }
+    ASSERT_TRUE(brokerExe.isActive());
+    auto res = brokerExe.runAsync(brokerArgs);
+
+    // give the helics_broker some time to start
+    std::this_thread::sleep_for(std::chrono::milliseconds(40));
+
+    const std::string fedArgs =
+        "--core_type=tcp --encrypted --encryption_config=" + std::string(TEST_BIN_DIR) +
+        "encryption_config/openssl.json";
+    helics::FederateInfo fi_enc(fedArgs);
+    helics::ValueFederate fed1("fed1", fi_enc);
+
+    helics::ValueFederate fed2("fed2", fi_enc);
+
+    fed2.enterExecutingModeAsync();
+    fed1.enterExecutingMode();
+    fed2.enterExecutingModeComplete();
+
+    fed1.finalize();
+    fed2.finalize();
+    res.get();
+}
+
+TEST_F(network_tests, test_encrypted_tcpss)
+{
+    const std::string brokerArgs =
+        "-f 2 --core_type=tcp_ss --encrypted --encryption_config=" + std::string(TEST_BIN_DIR) +
+        "encryption_config/openssl.json";
+    exeTestRunner brokerExe(HELICS_BROKER_LOCATION, "helics_broker");
+    if (!brokerExe.isActive()) {
+        std::cout << " unable to locate helics_broker in " << HELICS_BROKER_LOCATION << std::endl;
+    }
+    ASSERT_TRUE(brokerExe.isActive());
+    auto res = brokerExe.runAsync(brokerArgs);
+
+    // give the helics_broker some time to start
+    std::this_thread::sleep_for(std::chrono::milliseconds(40));
+
+    const std::string fedArgs =
+        "--core_type=tcp_ss --encrypted --encryption_config=" + std::string(TEST_BIN_DIR) +
+        "encryption_config/openssl.json";
+    helics::FederateInfo fi_enc(fedArgs);
+    helics::ValueFederate fed1("fed1", fi_enc);
+
+    helics::ValueFederate fed2("fed2", fi_enc);
+
+    fed2.enterExecutingModeAsync();
+    fed1.enterExecutingMode();
+    fed2.enterExecutingModeComplete();
+
+    fed1.finalize();
+    fed2.finalize();
+    res.get();
+}
+
+TEST_F(network_tests, test_encrypted_bridge)
+{
+    const std::string brokerArgs = "-f 2 --core_type=multi --config=" + std::string(TEST_BIN_DIR) +
+        "encryption_config/multiBroker_encrypted_bridge.json --encrypted --encryption_config=" +
+        std::string(TEST_BIN_DIR) + "encryption_config/openssl.json";
+    exeTestRunner brokerExe(HELICS_BROKER_LOCATION, "helics_broker");
+    if (!brokerExe.isActive()) {
+        std::cout << " unable to locate helics_broker in " << HELICS_BROKER_LOCATION << std::endl;
+    }
+    ASSERT_TRUE(brokerExe.isActive());
+    auto res = brokerExe.runAsync(brokerArgs);
+
+    // give the helics_broker some time to start
+    std::this_thread::sleep_for(std::chrono::milliseconds(40));
+
+    const std::string fedArgs_enc =
+        "--core_type=tcp_ss --brokerport=30000 --encrypted --encryption_config=" +
+        std::string(TEST_BIN_DIR) + "encryption_config/openssl.json";
+    helics::FederateInfo fi_enc(fedArgs_enc);
+    helics::ValueFederate fed_enc("fed_encrypted", fi_enc);
+
+    const std::string fedArgs_unenc = "--core_type=tcp --brokerport=40000";
+    helics::FederateInfo fi_unenc(fedArgs_unenc);
+    helics::ValueFederate fed_unenc("fed_unencrypted", fi_unenc);
+
+    fed_unenc.enterExecutingModeAsync();
+    fed_enc.enterExecutingMode();
+    fed_unenc.enterExecutingModeComplete();
+
+    fed_enc.finalize();
+    fed_unenc.finalize();
+    res.get();
+}
+#    endif
 #endif
