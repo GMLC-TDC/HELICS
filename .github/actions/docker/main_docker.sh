ec136ebe56429fe6d7bf031164054f4684d9559e
Philip Top <phlptp@gmail.com>
2022-03-12 07:07:47 -0800

    remove github sha check for the docker builds

--- .github/actions/docker/main_docker.sh
+++ .github/actions/docker/main_docker.sh
@@ -1,9 +1,4 @@
 #! /bin/sh
-# Require a valid Git ref as an argument
-if [ "$#" -ne 1 ] || ! git rev-parse --verify "$1" >/dev/null; then
-    echo "Usage: $0 GIT_REF TAG_NAME" >&2
-    exit 1
-fi
 
 docker build . -f config/Docker/Dockerfile-HELICS-apps --tag "helics/helics:latest" || rv=1
 docker push "helics/helics:latest" || rv=1
--- .github/actions/docker/release_docker.sh
+++ .github/actions/docker/release_docker.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
-# Require a valid Git ref as an argument
-if [ "$#" -ne 2 ] || ! git rev-parse --verify "$1" >/dev/null; then
-    echo "Usage: $0 GIT_REF TAG_NAME" >&2
+# Require a tag name for the docker build image
+if [ "$#" -ne 1 ] then
+    echo "Usage: $0 TAG_NAME" >&2
     exit 1
 fi
 
-docker build . -f config/Docker/Dockerfile-HELICS-apps --tag "helics/helics:$2" || rv=1
-docker push "helics/helics:$2" || rv=1
+docker build . -f config/Docker/Dockerfile-HELICS-apps --tag "helics/helics:$1" || rv=1
+docker push "helics/helics:$1" || rv=1
 
 exit $rv
