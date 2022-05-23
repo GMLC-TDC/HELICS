19eb70cac7d313497aa5e8c800d54579dafd0acd
Philip Top <phlptp@gmail.com>
2022-05-23 11:55:17 -0700

    fix clang-tidy warning

--- src/helics/core/TimeCoordinator.cpp
+++ src/helics/core/TimeCoordinator.cpp
@@ -1036,17 +1036,13 @@ MessageProcessingResult TimeCoordinator::checkExecEntry(GlobalFederateId trigger
             if (hasInitUpdates) {
                 ret = MessageProcessingResult::ITERATING;
             } else {
-                if (dependencies.checkIfReadyForExecEntry(false,
-                                                          info.wait_for_current_time_updates)) {
-                    ret = (iterating == IterationRequest::FORCE_ITERATION) ?
-                        MessageProcessingResult::ITERATING :
-                        MessageProcessingResult::NEXT_STEP;
-                } else if (info.wait_for_current_time_updates && dependencies.checkAllPastExec(true)) {
+                if ((dependencies.checkIfReadyForExecEntry(false,
+                                                           info.wait_for_current_time_updates)) ||
+                    (info.wait_for_current_time_updates && dependencies.checkAllPastExec(true))) {
                     ret = (iterating == IterationRequest::FORCE_ITERATION) ?
                         MessageProcessingResult::ITERATING :
                         MessageProcessingResult::NEXT_STEP;
                 } else {
-
                     bool allowed{!info.wait_for_current_time_updates};
                     bool restricted{info.restrictive_time_policy};
                     bool restrictionAdvance{restricted};
