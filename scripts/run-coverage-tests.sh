d2ae2e397afd830b620c781becf61fcfe8f4de6d
Philip Top <phlptp@gmail.com>
2022-05-24 07:28:46 -0700

    fix the coverage test scripts

--- scripts/run-coverage-tests.sh
+++ scripts/run-coverage-tests.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 tests=(
-    /root/project/build/bin/core-tests
-    /root/project/build/bin/common-tests
-    "/root/project/build/bin/system-tests --gtest_filter=*"
-    /root/project/build/bin/helics_apps-tests
-    "/root/project/build/bin/shared-library-tests-cpp --gtest_filter=-*death*"
-    "/root/project/build/bin/shared-library-tests --gtest_filter=-*death*"
-    "/root/project/build/bin/application-api-tests --gtest_filter=*"
-    "/root/project/build/bin/helics_wevserver-tests --gtest_filter=*"
-    "/root/project/build/bin/network-tests --gtest_filter=*"
+    ./bin/core-tests
+    ./bin/common-tests
+    "./bin/system-tests --gtest_filter=*"
+    ./bin/helics_apps-tests
+    "./bin/shared-library-tests-cpp --gtest_filter=-*death*"
+    "./bin/shared-library-tests --gtest_filter=-*death*"
+    "./bin/application-api-tests --gtest_filter=*"
+    "./bin/helics_webserver-tests --gtest_filter=*"
+    "./bin/network-tests --gtest_filter=*"
 )
 
 SUMRESULT=0
