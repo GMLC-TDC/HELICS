58f44b617cb9e846d93c00b33ab48380b31effb6
Ryan Mast <3969255+nightlark@users.noreply.github.com>
2022-09-15 14:41:26 -0700

    Disable VCPKG_ROOT in GHA and Azure builds (#2436)
    * Unset VCPKG_ROOT for builds in GitHub Actions workflows
    
    * Bump macOS SDK version for benchmark package build
    
    * Disable use of VCPKG_ROOT on Azure Pipelines as well
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove publishing of build folder as an artifact on Windows builds
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- .github/actions/release-build/installer-macOS.sh
+++ .github/actions/release-build/installer-macOS.sh
@@ -2,6 +2,9 @@
 # This uses bash variable substitution in a few places
 # 1. getting the cmake directory for running cpack with an absolute path (chocolatey has an unfortunately named alias)
 
+# Unset VCPKG_ROOT for GitHub actions environment
+unset VCPKG_ROOT
+
 brew install swig boost
 mkdir build && cd build || exit
 cmake -DCMAKE_BUILD_TYPE=Release -DHELICS_ZMQ_SUBPROJECT=ON -DHELICS_ENABLE_PACKAGE_BUILD=ON -DHELICS_BUILD_JAVA_INTERFACE=ON -DHELICS_BUILD_EXAMPLES=OFF -DHELICS_BUILD_APP_EXECUTABLES=ON -DHELICS_BUILD_APP_LIBRARY=ON -DBUILD_TESTING=OFF -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" ..
