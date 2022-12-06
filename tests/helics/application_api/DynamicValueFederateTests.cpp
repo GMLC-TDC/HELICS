23b49e809ee10b2c5827aac5dba3f5d13b364f3f
Philip Top <phlptp@gmail.com>
2022-12-06 05:30:17 -0800

    more codacy fixes

--- tests/helics/application_api/DynamicValueFederateTests.cpp
+++ tests/helics/application_api/DynamicValueFederateTests.cpp
@@ -256,7 +256,7 @@ TEST_F(dynFed, dynamicPubSubs_2fed_pubStore)
 
     auto& in2 = vFed1->registerSubscription("pub2");
 
-    auto res = vFed2->query("root", "global_flush");
+    vFed2->query("root", "global_flush");
     vFed1->requestTimeAsync(helics::timeZero);
     vFed2->requestNextStep();
     auto tres = vFed1->requestTimeComplete();
