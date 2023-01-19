5de2806e7aa0d5e43dbb037c0c6cc87ea84869dd
Ryan Mast <mast9@llnl.gov>
2023-01-19 13:23:00 -0800

    Add support for environment variables instead of command line arguments for encryption settings

--- src/helics/application_api/FederateInfo.cpp
+++ src/helics/application_api/FederateInfo.cpp
@@ -595,10 +595,12 @@ std::unique_ptr<helicsCLI11App> FederateInfo::makeCLIApp()
            "The minimum time between time grants for a Federate (default in ms)")
         ->configurable(false);
     auto* encrypt_group = app->add_option_group("encryption", "options related to encryption");
-    encrypt_group->add_flag("--encrypted", encrypted, "enable encryption on the network");
+    encrypt_group->add_flag("--encrypted", encrypted, "enable encryption on the network")
+        ->envname("HELICS_ENCRYPTION");
     encrypt_group->add_option("--encryption_config",
                               encryptionConfig,
-                              "set the configuration file for encryption options");
+                              "set the configuration file for encryption options")
+        ->envname("HELICS_ENCRYPTION_CONFIG");
 
     auto* rtgroup = app->add_option_group("realtime");
     rtgroup->option_defaults()->ignore_underscore();
