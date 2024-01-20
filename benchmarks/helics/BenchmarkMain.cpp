45e72d5d0cea2a604bc9ae3444a9f615ac1bf5ef
Philip Top <phlptp@gmail.com>
2024-01-19 17:46:02 -0800

    update dates to 2024

--- benchmarks/helics/BenchmarkMain.cpp
+++ benchmarks/helics/BenchmarkMain.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2017-2023,
+Copyright (c) 4,
 Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance for Sustainable
 Energy, LLC.  See the top-level NOTICE for additional details. All rights reserved.
 SPDX-License-Identifier: BSD-3-Clause
