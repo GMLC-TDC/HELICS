bcc3cf409e4b8dc9987a6c86d67d5950b8381dcc
Philip Top <top1@llnl.gov>
2024-01-24 06:08:10 -0800

    update dates to 2024 (#2595)
    * update dates to 2024
    
    * Apply suggestions from code review
    
    Co-authored-by: Ryan Mast <3969255+nightlark@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ryan Mast <3969255+nightlark@users.noreply.github.com>

--- scripts/generateHelicsH.py
+++ scripts/generateHelicsH.py
@@ -24,7 +24,7 @@ if len(sys.argv) > 2:
 outfile = open(os.path.join(outpath, "helics.h"), "w")
 
 outfile.write("/*\n")
-outfile.write("Copyright (c) 2017-2023,\n")
+outfile.write("Copyright (c) 2017-2024,\n")
 outfile.write(
     "Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance for Sustainable Energy, LLC.  See the top-level NOTICE for\n"
 )
@@ -111,7 +111,7 @@ outfile.close()
 outfile2 = open(os.path.join(outpath, "helics_api.h"), "w")
 
 outfile2.write("/*\n")
-outfile2.write("Copyright (c) 2017-2023,\n")
+outfile2.write("Copyright (c) 2017-2024,\n")
 outfile2.write(
     "Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance for Sustainable Energy, LLC.  See the top-level NOTICE for\n"
 )
