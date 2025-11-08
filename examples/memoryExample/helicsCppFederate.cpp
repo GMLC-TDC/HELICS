7df574d7e7f2b1d658893b417fec14b3529e190a
Philip Top <phlptp@gmail.com>
2025-11-08 09:29:27 -0800

    remove std::format

--- examples/memoryExample/helicsCppFederate.cpp
+++ examples/memoryExample/helicsCppFederate.cpp
@@ -271,11 +271,10 @@ class HelicsCppFederate {
                     currentTime = federate->requestTime(currentTime + 1.0);
                     int completionStatus = static_cast<int>(currentTime * 100 / endTime);
                     fedLog->log(LogLevel::INFO,
-                                std::format("Cpp Federate Status: {}% complete.",
-                                            completionStatus));
+                        std::string("Cpp Federate Status: ")+std::to_string(completionStatus)+"% complete.");
                 } else {
                     fedLog->log(LogLevel::INFO,
-                                std::string("Cpp Federate Status: Simulation complete.\n"));
+                        std::string("Cpp Federate Status: Simulation complete.\n"));
                     break;
                 }
             } else {
