c21cbde690dc50f13d671307e1abda5c96481f0e
Philip Top <top1@llnl.gov>
2026-06-07 05:38:55 -0700

    Ci updates (#2789)
    * update requirements for docs
    
    * remove windows-2019 builds
    
    * update macos images
    
    * updates to readthedocs
    
    * try to fix some of the failing tests
    
    * more precommit cleanup
    
    * more resolutions
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * tabs to spaces
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * try to exclude pkgbuild from shfmt
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * try docs build with 3.12
    
    * try python 3.11 with read the docs
    
    * try to resolve readthedocs build
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * try removing one requirement
    
    * update for some warnings and cmake options
    
    * update clang-tidy file
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/helics/helics98.hpp
+++ src/helics/helics98.hpp
@@ -8,11 +8,11 @@ SPDX-License-Identifier: BSD-3-Clause
 /** @file
 Header file for inclusion of the entire CPP shared library
 */
-#ifndef SRC_HELICS_HELICS98_HPP_
-#define SRC_HELICS_HELICS98_HPP_
+#ifndef ___W_HELICS_HELICS_SRC_HELICS_HELICS98_HPP_
+#define ___W_HELICS_HELICS_SRC_HELICS_HELICS98_HPP_
 
 #include "cpp98/Broker.hpp"
 #include "cpp98/Core.hpp"
 #include "cpp98/helics.hpp"
 
-#endif  // SRC_HELICS_HELICS98_HPP_
+#endif  // ___W_HELICS_HELICS_SRC_HELICS_HELICS98_HPP_
