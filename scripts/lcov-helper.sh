c15fe48094b7200cf72ec95f22c6d672aa0d4d34
Philip Top <phlptp@gmail.com>
2026-06-21 05:43:13 -0700

    update the coverage build

--- scripts/lcov-helper.sh
+++ scripts/lcov-helper.sh
@@ -22,31 +22,22 @@ if [[ "$1" ]]; then
                 SUBMIT_COVERALLS=true
                 shift
                 ;;
-            --codecov)
-                SUBMIT_CODECOV=true
-                shift
-                ;;
             *)
                 shift
                 ;;
             esac
         done
 
-        if [[ "$SUBMIT_CODECOV" != "true" || "$SUBMIT_COVERALLS" == "true" ]]; then
-            # Get the coverage info from the test runs into a file
-            lcov --gcov-tool "$GCOV_TOOL" --no-external --directory . --capture --output-file coverage.info &>/dev/null
-            # Combine the base coverage info with info from running programs
-            lcov -a coverage.base -a coverage.info --output-file coverage.total >/dev/null
-            # Clean-up the coverage info
-            lcov --remove coverage.total 'test/*' 'tests/*' 'ThirdParty/*' 'dependencies/*' '/usr/*' --output-file coverage.info.cleaned >/dev/null
-        fi
+        # Get the coverage info from the test runs into a file
+        lcov --gcov-tool "$GCOV_TOOL" --no-external --directory . --capture --output-file coverage.info &>/dev/null
+        # Combine the base coverage info with info from running programs
+        lcov -a coverage.base -a coverage.info --output-file coverage.total >/dev/null
+        # Clean-up the coverage info
+        lcov --remove coverage.total 'test/*' 'tests/*' 'ThirdParty/*' 'dependencies/*' '/usr/*' --output-file coverage.info.cleaned >/dev/null
 
         # Submit coverage info to dashboard
         if [[ "$SUBMIT_COVERALLS" == "true" ]]; then
             coveralls --gcov "${GCOV_TOOL}" --lcov-file coverage.info.cleaned >/dev/null
-        fi
-        if [[ "$SUBMIT_CODECOV" == "true" ]]; then
-            bash <(curl -s https://codecov.io/bash) -x "${GCOV_TOOL}" >/dev/null
         fi
         ;;
     *)
