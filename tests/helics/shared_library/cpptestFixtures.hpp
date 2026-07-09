a9152ae15f3a5366d651fb330b631c4d8c085c89
Philip Top <phlptp@gmail.com>
2026-07-09 07:10:01 -0700

    clang-tidy fixes and disabling asio on windows

--- tests/helics/shared_library/cpptestFixtures.hpp
+++ tests/helics/shared_library/cpptestFixtures.hpp
@@ -136,6 +136,6 @@ struct FederateTestFixture_cpp {
     std::string ctype;
 
   private:
-    bool hasIndexCode(const std::string& type_name);
-    int getIndexCode(const std::string& type_name);
+    static bool hasIndexCode(const std::string& type_name);
+    static int getIndexCode(const std::string& type_name);
 };
