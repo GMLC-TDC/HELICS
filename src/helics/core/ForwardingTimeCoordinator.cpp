50a20cbc60d640f3707cb806de47d73e908e546c
Philip Top <phlptp@gmail.com>
2026-07-24 17:23:46 -0700

    add a condition for current time flag on disconnect with forwarding time coordinator

--- src/helics/core/ForwardingTimeCoordinator.cpp
+++ src/helics/core/ForwardingTimeCoordinator.cpp
@@ -20,6 +20,28 @@ SPDX-License-Identifier: BSD-3-Clause
 
 namespace helics {
 
+static DependencyInfo generateDelayedDependencyInfo(const TimeDependencies& dependencies,
+                                                    bool restrictiveTimePolicy,
+                                                    GlobalFederateId sourceId,
+                                                    GlobalFederateId delayedFed)
+{
+    auto td = generateMinTimeUpstream(
+        dependencies, restrictiveTimePolicy, sourceId, delayedFed, 0);
+    const bool noPendingUpstream =
+        std::none_of(dependencies.begin(), dependencies.end(), [sourceId, delayedFed](const auto& dep) {
+            return dep.dependency && dep.fedID != delayedFed &&
+                dep.connection != ConnectionType::PARENT &&
+                (!sourceId.isValid() || dep.minFedActual != sourceId) &&
+                (dep.mTimeState == TimeState::error || dep.next < cTerminationTime);
+        });
+    if (td.mTimeState == TimeState::error && noPendingUpstream) {
+        return DependencyInfo(Time::maxVal(), TimeState::time_granted);
+    }
+    DependencyInfo dependency;
+    dependency.update(td);
+    return dependency;
+}
+
 bool ForwardingTimeCoordinator::updateTimeFactors()
 {
     auto mTimeUpstream = generateMinTimeUpstream(
@@ -69,10 +91,8 @@ bool ForwardingTimeCoordinator::updateTimeFactors()
             if (upd.action() != CMD_IGNORE) {
                 transmitTimingMessagesDownstream(upd, downstream.minFed);
             }
-            auto td = generateMinTimeUpstream(
-                dependencies, restrictive_time_policy, mSourceId, downstream.minFed, 0);
-            DependencyInfo dependency;
-            dependency.update(td);
+            auto dependency = generateDelayedDependencyInfo(
+                dependencies, restrictive_time_policy, mSourceId, downstream.minFed);
             auto upd_delayed = generateTimeRequest(dependency,
                                                    downstream.minFed,
                                                    dependency.responseSequenceCounter);
@@ -87,10 +107,8 @@ bool ForwardingTimeCoordinator::updateTimeFactors()
         }
     } else if (dependencies.hasDelayedDependency() &&
                mTimeDownstream.minFed == dependencies.delayedDependency() && executionMode) {
-        auto minTimeUpstream = generateMinTimeUpstream(
-            dependencies, restrictive_time_policy, mSourceId, mTimeDownstream.minFed, 0);
-        DependencyInfo dependency;
-        dependency.update(minTimeUpstream);
+        auto dependency = generateDelayedDependencyInfo(
+            dependencies, restrictive_time_policy, mSourceId, mTimeDownstream.minFed);
         auto upd_delayed = generateTimeRequest(dependency,
                                                mTimeDownstream.minFed,
                                                dependency.responseSequenceCounter);
